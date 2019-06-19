8fd87ab6a6d4b97760e0b0df79a51fd6cda60923
Sachin Umesh Naik <sachin.u.naik@gmail.com>
2019-06-18 21:11:42 -0700

    Fix for linux build

--- _util/build_linux.mk
+++ _util/build_linux.mk
@@ -7,9 +7,13 @@ $(error PLATFORM doesn't match linux)
 endif
 
 build-tools:
-	@echo ">>> Installing build-essentials tools"
-	sudo apt-get -y install build-essential
-	@echo ">>> Install go tools"
-	sudo snap install go --classic
-	@echo ">>> Display go version"
-	go version
+	@echo ">>> Update apt"
+	sudo apt update
+	@echo ">>> Install jq"
+   	sudo apt-get install jq
+   	@echo ">>> Installing build-essentials tools"
+   	sudo apt-get -y install build-essential
+   	@echo ">>> Install go tools"
+   	sudo snap install go --classic
+   	@echo ">>> Display go version"
+   	go version
