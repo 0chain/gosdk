c114c83174b07879f5d407b252d8da0301a46e02
Sculptex <sculptex@users.noreply.github.com>
2019-09-12 21:55:14 +0100

    Update build_linux.mk
    Added Yum package manager based distro support

--- _util/build_linux.mk
+++ _util/build_linux.mk
@@ -6,7 +6,26 @@ ifneq ($(PLATFORM), linux)
 $(error PLATFORM doesn't match linux)
 endif
 
+REDHATOS := $(shell command cat /etc/redhat-release 2> /dev/null)
+
 build-tools:
+ifdef REDHATOS
+	@echo ">>> Update apt"
+	yum update
+	@echo ">>> Install jq"
+	yum install -y epel-release
+	yum install -y jq
+	@echo ">>> Installing build-essentials tools"
+	yum install -y gcc gcc-c++ kernel-devel make
+	yum groupinstall -y "Development Tools"
+	@echo ">>> Install go tools"
+	yum install -y wget
+	wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+	tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
+	@echo ">>> ADD GO BIN PATH to path manually, then test go"
+	@echo "export PATH=$PATH:/usr/local/go/bin"
+	@echo "go version"
+else
 	@echo ">>> Update apt"
 	sudo apt update
 	@echo ">>> Install jq"
@@ -17,3 +36,4 @@ build-tools:
 	sudo snap install go --classic
 	@echo ">>> Display go version"
 	go version
+endif
