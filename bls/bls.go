55177b3eed16cae639ddb0675af9d5d303ce52e6
Alex Guo <chessnut@outlook.com>
2021-01-19 05:42:20 -0800

    port over bls.Sign.DeserializeHexStr

--- bls/bls.go
+++ bls/bls.go
@@ -2,6 +2,7 @@ package bls
 
 import (
   "fmt"
+  "encoding/hex"
   "github.com/0chain/gosdk/miracl"
 )
 
@@ -10,7 +11,27 @@ import (
 // blsSignature: <https://github.com/herumi/bls/blob/1b48de51f4f76deb204d108f6126c1507623f739/include/bls/bls.h#L68>
 // mclBnG1: <https://github.com/herumi/mcl/blob/0114a3029f74829e79dc51de6dfb28f5da580632/include/mcl/bn.h#L96>
 type Sign struct {
-  v BN254.ECP
+  v *BN254.ECP
+}
+
+// Taken directly from herumi's library:
+// <https://github.com/herumi/bls-go-binary/blob/ef6a150a928bddb19cee55aec5c80585528d9a96/bls/bls.go#L29>
+func hex2byte(s string) ([]byte, error) {
+	if (len(s) & 1) == 1 {
+		return nil, fmt.Errorf("odd length")
+	}
+	return hex.DecodeString(s)
+}
+
+// Starting from herumi's library:
+// <https://github.com/herumi/bls-go-binary/blob/ef6a150a928bddb19cee55aec5c80585528d9a96/bls/bls.go#L480>
+func (sig *Sign) DeserializeHexStr(s string) error {
+  b, err := hex2byte(s)
+	if err != nil {
+		return err
+	}
+  sig.v = BN254.ECP_fromBytes(b)
+  return nil
 }
 
 func Init() {
