d0493ff8d1ffb083d6a4bcf8f0a9021552c0e0ee
Alex Guo <chessnut@outlook.com>
2021-02-13 13:05:16 -0800

    Add unit test to sanity check serialization/deserialization of the ported library

--- bls/bls.go
+++ bls/bls.go
@@ -74,7 +74,6 @@ func ToBytes(E *BN254.ECP) []byte {
 	const BFS = BN254.BFS
 	const G1S = BFS + 1 /* Group 1 Size */
 	var ecp [G1S]byte
-	fmt.Println("aa2", G1S)
 	E.ToBytes(ecp[:], true /*compress*/)
 	return ecp[:]
 }
@@ -224,6 +223,12 @@ type SecretKey struct {
 	v *BN254.FP
 }
 
+func NewSecretKey() *SecretKey {
+  sk := new(SecretKey)
+  sk.v = BN254.NewFP()
+  return sk
+}
+
 func SecretKey_fromBytes(b []byte) *SecretKey {
 	sk := new(SecretKey)
 	sk.v = BN254.FP_fromBytes(b)
@@ -264,8 +269,8 @@ func (sk *SecretKey) SetByCSPRNG() error {
 			panic("Couldn't read from sRandReader. Got an error (printed out on previous lines.")
 		}
 	}
-	z := len(b)
-	b[z-1] = b[z-1] & 0x1f
+	// z := len(b)
+	// b[z-1] = b[z-1] & 0x1f
 	w = BN254.FromBytes(b)
 	w.ToBytes(b)
 	sk.v = BN254.NewFPbigcopy(w)
