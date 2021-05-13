0a357213649059fdbca5323eed90634605b5038f
Alex Guo <chessnut@outlook.com>
2021-05-13 02:32:03 -0500

    proxy.go: wrap Move in go routine

--- _sdkver/proxy.go
+++ _sdkver/proxy.go
@@ -668,25 +668,41 @@ func Move(this js.Value, p []js.Value) interface{} {
 		return js.ValueOf("error: " + NewError("invalid_param", "Please provide remote_path and dest_path for move").Error())
 	}
 
-	err = initSDK(clientJSON)
-	if err != nil {
-		return js.ValueOf("error: " + NewError("sdk_not_initialized", "Unable to initialize gosdk with the given client details").Error())
-	}
+	handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		resolve := args[0]
+		reject := args[1]
 
-	allocationObj, err := sdk.GetAllocation(allocation)
-	if err != nil {
-		return js.ValueOf("error: " + NewError("get_allocation_failed", err.Error()).Error())
-	}
+		go func() {
+			err = initSDK(clientJSON)
+			if err != nil {
+				reject.Invoke(js.ValueOf("error: " + NewError("sdk_not_initialized", "Unable to initialize gosdk with the given client details").Error()))
+			}
 
-	err = allocationObj.MoveObject(remotePath, destPath)
-	if err != nil {
-		return js.ValueOf("error: " + NewError("move_object_failed", err.Error()).Error())
-	}
+			allocationObj, err := sdk.GetAllocation(allocation)
+			if err != nil {
+				reject.Invoke(js.ValueOf("error: " + NewError("get_allocation_failed", err.Error()).Error()))
+			}
 
-	// resp := response{
-	// 	Message: "Move done successfully",
-	// }
-	return js.ValueOf("Move done successfully")
+			err = allocationObj.MoveObject(remotePath, destPath)
+			if err != nil {
+				reject.Invoke(js.ValueOf("error: " + NewError("move_object_failed", err.Error()).Error()))
+			}
+
+			responseConstructor := js.Global().Get("Response")
+			response := responseConstructor.New(js.ValueOf("Move done successfully"))
+
+			// resp := response{
+			// 	Message: "Move done successfully",
+			// }
+			resolve.Invoke(response)
+		}()
+
+		return nil
+	})
+
+	// Create and return the Promise object
+	promiseConstructor := js.Global().Get("Promise")
+	return promiseConstructor.New(handler)
 }
 
 //-----------------------------------------------------------------------------
