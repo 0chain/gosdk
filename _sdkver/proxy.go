f6949a53874dbc02eafdb87fe76344cfd8d58cf0
Alex Guo <chessnut@outlook.com>
2021-05-13 02:28:22 -0500

    proxy.go: wrap Copy in go routine

--- _sdkver/proxy.go
+++ _sdkver/proxy.go
@@ -517,22 +517,39 @@ func Copy(this js.Value, p []js.Value) interface{} {
 		return js.ValueOf("error: " + NewError("invalid_param", "Please provide remote_path and dest_path for copy").Error())
 	}
 
-	err = initSDK(clientJSON)
-	if err != nil {
-		return js.ValueOf("error: " + NewError("sdk_not_initialized", "Unable to initialize gosdk with the given client details").Error())
-	}
+	handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		resolve := args[0]
+		reject := args[1]
 
-	allocationObj, err := sdk.GetAllocation(allocation)
-	if err != nil {
-		return js.ValueOf("error: " + NewError("get_allocation_failed", err.Error()).Error())
-	}
+		go func() {
+			err = initSDK(clientJSON)
+			if err != nil {
+				reject.Invoke(js.ValueOf("error: " + NewError("sdk_not_initialized", "Unable to initialize gosdk with the given client details").Error()))
+			}
 
-	err = allocationObj.CopyObject(remotePath, destPath)
-	if err != nil {
-		return js.ValueOf("error: " + NewError("copy_object_failed", err.Error()).Error())
-	}
+			allocationObj, err := sdk.GetAllocation(allocation)
+			if err != nil {
+				reject.Invoke(js.ValueOf("error: " + NewError("get_allocation_failed", err.Error()).Error()))
+			}
 
-	return "Copy done successfully"
+			err = allocationObj.CopyObject(remotePath, destPath)
+			if err != nil {
+				reject.Invoke(js.ValueOf("error: " + NewError("copy_object_failed", err.Error()).Error()))
+			}
+
+			responseConstructor := js.Global().Get("Response")
+			response := responseConstructor.New(js.ValueOf("Copy done successfully"))
+
+			// Resolve the Promise
+			resolve.Invoke(response)
+		}()
+
+		return nil
+	})
+
+	// Create and return the Promise object
+	promiseConstructor := js.Global().Get("Promise")
+	return promiseConstructor.New(handler)
 }
 
 //-----------------------------------------------------------------------------
