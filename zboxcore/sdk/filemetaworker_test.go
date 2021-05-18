3abd1a36f07ab639e736195ea8ee416c903e0089
Vu Dang <dnvu.24h@gmail.com>
2021-05-18 16:07:24 +0700

    Update test cases

--- zboxcore/sdk/filemetaworker_test.go
+++ zboxcore/sdk/filemetaworker_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/0chain/gosdk/zboxcore/marker"
+	"github.com/0chain/gosdk/zboxcore/zboxutil"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,40 +21,40 @@ func TestListRequest_getFileMetaInfoFromBlobber(t *testing.T) {
 	// setup mock allocation
 	a, cncl := setupMockAllocation(t, filemetaWorkerTestDir, blobberMocks)
 	var blobbersResponseMock = func(t *testing.T, testcaseName string) (teardown func(t *testing.T)) {
-		setupBlobberMockResponses(t, blobberMocks, filemetaWorkerTestDir+"/getFileConsensusFromBlobbers", testcaseName)
+		setupBlobberMockResponses(t, blobberMocks, filemetaWorkerTestDir+"/getFileMetaInfoFromBlobber", testcaseName)
 		return nil
 	}
 	defer cncl()
 	tests := []struct {
 		name           string
 		additionalMock func(t *testing.T, testCaseName string) (teardown func(t *testing.T))
-		wantFunc       func(assertions *assert.Assertions, ar *ListRequest)
+		want           bool
+		wantErr        bool
 	}{
 		{
-			name: "Test_Success",
-			additionalMock: func(t *testing.T, testCaseName string) (teardown func(t *testing.T)) {
-				blobbersResponseMock(t, testCaseName)
-				willReturnCommitResult(&CommitResult{Success: true})
-				return nil
+			"Test_Error_New_HTTP_Failed",
+			func(t *testing.T, testCaseName string) (teardown func(t *testing.T)) {
+				url := a.Blobbers[0].Baseurl
+				a.Blobbers[0].Baseurl = string([]byte{0x7f, 0, 0})
+				return func(t *testing.T) {
+					a.Blobbers[0].Baseurl = url
+				}
 			},
-			wantFunc: func(assertions *assert.Assertions, ar *ListRequest) {
-				assertions.NotNil(ar)
-				assertions.Equal(float32(0), ar.consensus)
-			},
+			false,
+			true,
 		},
-		// {
-		// 	name: "Test_All_Blobber_Delete_False",
-		// 	additionalMock: func(t *testing.T, testCaseName string) (teardown func(t *testing.T)) {
-		// 		blobbersResponseMock(t, testCaseName)
-		// 		willReturnCommitResult(&CommitResult{Success: true})
-		// 		return nil
-		// 	},
-		// 	wantFunc: func(assertions *assert.Assertions, ar *DeleteRequest) {
-		// 		assertions.NotNil(ar)
-		// 		assertions.Equal(uint32(0), ar.deleteMask)
-		// 		assertions.Equal(float32(0), ar.consensus)
-		// 	},
-		// },
+		{
+			"Test_Failed",
+			blobbersResponseMock,
+			false,
+			false,
+		},
+		{
+			"Test_Success",
+			blobbersResponseMock,
+			true,
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -74,56 +75,60 @@ func TestListRequest_getFileMetaInfoFromBlobber(t *testing.T) {
 				},
 				authToken: &marker.AuthTicket{
 					ClientID: "",
-					OwnerID: "",
+					OwnerID:  "",
 				},
 				ctx: context.Background(),
 			}
 			rspCh := make(chan *fileMetaResponse, 1)
-
-			// objectTreeRefs := make([]fileref.RefEntity, 1)
-
 			req.wg = &sync.WaitGroup{}
 			req.wg.Add(1)
 			go req.getFileMetaInfoFromBlobber(req.blobbers[0], 0, rspCh)
 			req.wg.Wait()
-
-			if tt.wantFunc != nil {
-				tt.wantFunc(assertion, req)
+			resp := <-rspCh
+			if !tt.want {
+				assertion.Nil(resp.fileref)
+			}
+			if tt.wantErr {
+				assertion.Error(resp.err, "expected error != nil")
 			}
 		})
 	}
 }
 
-func TestListRequest_getFileMetaFromBlobbers(t *testing.T) {
+func TestListRequest_getFileConsensusFromBlobbers(t *testing.T) {
 	// setup mock sdk
 	_, _, blobberMocks, closeFn := setupMockInitStorageSDK(t, configDir, 4)
 	defer closeFn()
 	// setup mock allocation
 	a, cncl := setupMockAllocation(t, filemetaWorkerTestDir, blobberMocks)
 	var blobbersResponseMock = func(t *testing.T, testcaseName string) (teardown func(t *testing.T)) {
-		setupBlobberMockResponses(t, blobberMocks, filemetaWorkerTestDir+"/getFileMetaFromBlobbers", testcaseName)
+		setupBlobberMockResponses(t, blobberMocks, filemetaWorkerTestDir+"/getFileConsensusFromBlobbers", testcaseName)
 		return nil
 	}
 	defer cncl()
 	tests := []struct {
-		name            string
-		additionalMock  func(t *testing.T, testCaseName string) (teardown func(t *testing.T))
-		wantFunc        func(assertions *assert.Assertions, ar *ListRequest)
-		wantErr         bool
-		wantErrContains string
+		name           string
+		additionalMock func(t *testing.T, testCaseName string) (teardown func(t *testing.T))
+		wantFound      uint64
+		wantErr        bool
 	}{
 		{
-			name: "Test_Success",
-			additionalMock: func(t *testing.T, testCaseName string) (teardown func(t *testing.T)) {
-				blobbersResponseMock(t, testCaseName)
-				willReturnCommitResult(&CommitResult{Success: true})
-				return nil
-			},
-			wantErr: false,
-			wantFunc: func(assertions *assert.Assertions, ar *ListRequest) {
-				assertions.NotNil(ar)
-				assertions.Equal(float32(0), ar.consensus)
-			},
+			"Test_All_Success",
+			blobbersResponseMock,
+			0xf,
+			false,
+		},
+		{
+			"Test_Index_3_Error",
+			blobbersResponseMock,
+			0x7,
+			false,
+		},
+		{
+			"Test_File_Consensus_Not_Found",
+			blobbersResponseMock,
+			0x0,
+			true,
 		},
 	}
 	for _, tt := range tests {
@@ -139,88 +144,15 @@ func TestListRequest_getFileMetaFromBlobbers(t *testing.T) {
 				allocationTx:   a.Tx,
 				blobbers:       a.Blobbers,
 				remotefilepath: "/1.txt",
+				ctx:            context.Background(),
 				Consensus: Consensus{
 					consensusThresh: (float32(a.DataShards) * 100) / float32(a.DataShards+a.ParityShards),
 					fullconsensus:   float32(a.DataShards + a.ParityShards),
 				},
-				ctx:          context.Background(),
-				// connectionID: zboxutil.NewConnectionId(),
 			}
 
-			req.getFileMetaFromBlobbers()
-			if tt.wantFunc != nil {
-				tt.wantFunc(assertion, req)
-			}
-		})
-	}
-}
-func TestListRequest_getFileConsensusFromBlobbers(t *testing.T) {
-	// setup mock sdk
-	_, _, blobberMocks, closeFn := setupMockInitStorageSDK(t, configDir, 1)
-	defer closeFn()
-	// setup mock allocation
-	a, cncl := setupMockAllocation(t, filemetaWorkerTestDir, blobberMocks)
-	var blobbersResponseMock = func(t *testing.T, testcaseName string) (teardown func(t *testing.T)) {
-		setupBlobberMockResponses(t, blobberMocks, filemetaWorkerTestDir+"/getFileMetaInfoFromBlobber", testcaseName)
-		return nil
-	}
-	defer cncl()
-	tests := []struct {
-		name            string
-		additionalMock  func(t *testing.T, testCaseName string) (teardown func(t *testing.T))
-		wantFunc        func(assertions *assert.Assertions, ar *ListRequest)
-		wantErr         bool
-		wantErrContains string
-		remotefilepath  string
-	}{
-		{
-			name: "Test_Success",
-			additionalMock: func(t *testing.T, testCaseName string) (teardown func(t *testing.T)) {
-				blobbersResponseMock(t, testCaseName)
-				willReturnCommitResult(&CommitResult{Success: true})
-				return nil
-			},
-			wantFunc: func(assertions *assert.Assertions, ar *ListRequest) {
-				assertions.NotNil(ar)
-				assertions.Equal(float32(1), ar.consensus)
-			},
-			remotefilepath: "1.txt",
-		},
-		{
-			name: "Test_False",
-			additionalMock: func(t *testing.T, testCaseName string) (teardown func(t *testing.T)) {
-				blobbersResponseMock(t, testCaseName)
-				willReturnCommitResult(&CommitResult{Success: true})
-				return nil
-			},
-			wantFunc: func(assertions *assert.Assertions, ar *ListRequest) {
-				assertions.NotNil(ar)
-				assertions.Equal(float32(0), ar.consensus)
-			},
-			remotefilepath: "1.txt",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assertion := assert.New(t)
-			if additionalMock := tt.additionalMock; additionalMock != nil {
-				if teardown := additionalMock(t, tt.name); teardown != nil {
-					defer teardown(t)
-				}
-			}
-			req := &ListRequest{
-				allocationID:   a.ID,
-				allocationTx:   a.Tx,
-				blobbers:       a.Blobbers,
-				remotefilepath: tt.remotefilepath,
-				ctx:            context.Background(),
-			}
-	
-			req.wg = &sync.WaitGroup{}
-			req.getFileConsensusFromBlobbers()
-			if tt.wantFunc != nil {
-				tt.wantFunc(assertion, req)
-			}
+			found, _, _ := req.getFileConsensusFromBlobbers()
+			assertion.Equal(zboxutil.NewUint128(tt.wantFound), found, "found value must be same")
 		})
 	}
 }
