f0684648562eae324c003792d5b5097288e05da3
Vu Dang <dnvu.24h@gmail.com>
2021-06-15 02:49:46 +0700

    add fields test for deleteBlobberFile func

--- zboxcore/sdk/deleteworker_test.go
+++ zboxcore/sdk/deleteworker_test.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"strconv"
 	"strings"
@@ -22,100 +24,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDeleteRequest_getObjectTreeFromBlobber(t *testing.T) {
-	const (
-		mockAllocationTxId = "mock transaction id"
-		mockClientId       = "mock client id"
-		mockClientKey      = "mock client key"
-		mockAllocationId   = "mock allocation id"
-		mockBlobberId      = "mock blobber id"
-		mockType           = "f"
-	)
-
-	var mockClient = mocks.HttpClient{}
-	zboxutil.Client = &mockClient
-
-	client := zclient.GetClient()
-	client.Wallet = &zcncrypto.Wallet{
-		ClientID:  mockClientId,
-		ClientKey: mockClientKey,
-	}
-
-	tests := []struct {
-		name    string
-		setup   func(*testing.T, string)
-		wantErr bool
-		errMsg  string
-	}{
-		{
-			name: "Test_Get_Object_Tree_From_Blobber_Failed",
-			setup: func(t *testing.T, testName string) {
-				mockClient.On("Do", mock.MatchedBy(func(req *http.Request) bool {
-					return strings.HasPrefix(req.URL.Path, testName)
-				})).Return(&http.Response{
-					StatusCode: http.StatusBadRequest,
-					Body:       ioutil.NopCloser(bytes.NewReader([]byte(""))),
-				}, nil)
-			},
-			wantErr: true,
-			errMsg:  "Object tree error response: Status: 400 -  ",
-		},
-		{
-			name: "Test_Get_Object_Tree_From_Blobber_Success",
-			setup: func(t *testing.T, testName string) {
-				mockClient.On("Do", mock.MatchedBy(func(req *http.Request) bool {
-					return strings.HasPrefix(req.URL.Path, testName)
-				})).Run(func(args mock.Arguments) {
-					for _, c := range mockClient.ExpectedCalls {
-						c.ReturnArguments = mock.Arguments{&http.Response{
-							StatusCode: http.StatusOK,
-							Body: func() io.ReadCloser {
-								jsonFR, err := json.Marshal(fileref.ReferencePath{
-									Meta: map[string]interface{}{
-										"type": mockType,
-									},
-								})
-								require.NoError(t, err)
-								return ioutil.NopCloser(bytes.NewReader([]byte(jsonFR)))
-							}(),
-						}, nil}
-					}
-				})
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			require := require.New(t)
-			tt.setup(t, tt.name)
-			blobber := &blockchain.StorageNode{
-				ID:      mockBlobberId,
-				Baseurl: tt.name,
-			}
-			req := &DeleteRequest{
-				allocationID: mockAllocationId,
-				allocationTx: mockAllocationTxId,
-				Consensus: Consensus{
-					consensusThresh: 50,
-					fullconsensus:   4,
-				},
-				ctx:          context.TODO(),
-				connectionID: zboxutil.NewConnectionId(),
-			}
-			objectTreeRefs := make([]fileref.RefEntity, 1)
-			refEntity, _ := req.getObjectTreeFromBlobber(blobber)
-			objectTreeRefs[0] = refEntity
-			_, err := req.getObjectTreeFromBlobber(blobber)
-			require.EqualValues(tt.wantErr, err != nil)
-			if err != nil {
-				require.EqualValues(tt.errMsg, err.Error())
-				return
-			}
-			require.NoErrorf(err, "expected no error but got %v", err)
-		})
-	}
-}
-
 func TestDeleteRequest_deleteBlobberFile(t *testing.T) {
 	const (
 		mockAllocationTxId = "mock transaction id"
@@ -125,6 +33,7 @@ func TestDeleteRequest_deleteBlobberFile(t *testing.T) {
 		mockAllocationId   = "mock allocation id"
 		mockBlobberId      = "mock blobber id"
 		mockType           = "f"
+		mockConnectionId   = "1234567890"
 	)
 
 	var mockClient = mocks.HttpClient{}
@@ -138,36 +47,50 @@ func TestDeleteRequest_deleteBlobberFile(t *testing.T) {
 
 	var wg sync.WaitGroup
 
+	type parameters struct {
+		referencePathToRetrieve fileref.ReferencePath
+		requestFields           map[string]string
+	}
+
 	tests := []struct {
-		name     string
-		setup    func(t *testing.T, name string)
-		wantFunc func(require *require.Assertions, req *DeleteRequest)
+		name       string
+		parameters parameters
+		setup      func(*testing.T, string, parameters)
+		wantFunc   func(*require.Assertions, *DeleteRequest)
 	}{
 		{
 			name: "Test_Delete_Blobber_File_Failed",
-			setup: func(t *testing.T, testName string) {
+			parameters: parameters{
+				referencePathToRetrieve: fileref.ReferencePath{
+					Meta: map[string]interface{}{
+						"type": mockType,
+					},
+				},
+			},
+			setup: func(t *testing.T, testName string, p parameters) {
 				mockClient.On("Do", mock.MatchedBy(func(req *http.Request) bool {
-					return req.Method == "GET" &&
-						strings.HasPrefix(req.URL.Path, testName)
+					return strings.HasPrefix(req.URL.Path, testName) &&
+						req.Method == "GET" &&
+						req.Header.Get("X-App-Client-ID") == mockClientId &&
+						req.Header.Get("X-App-Client-Key") == mockClientKey
 				})).Run(func(args mock.Arguments) {
 					for _, c := range mockClient.ExpectedCalls {
 						c.ReturnArguments = mock.Arguments{&http.Response{
 							StatusCode: http.StatusOK,
 							Body: func() io.ReadCloser {
-								jsonFR, err := json.Marshal(fileref.ReferencePath{
-									Meta: map[string]interface{}{
-										"type": mockType,
-									},
-								})
+								jsonFR, err := json.Marshal(p.referencePathToRetrieve)
 								require.NoError(t, err)
 								return ioutil.NopCloser(bytes.NewReader([]byte(jsonFR)))
 							}(),
 						}, nil}
 					}
 				})
+
 				mockClient.On("Do", mock.MatchedBy(func(req *http.Request) bool {
-					return req.Method == "DELETE" &&
-						strings.HasPrefix(req.URL.Path, testName)
+					return strings.HasPrefix(req.URL.Path, testName) &&
+						req.Method == "DELETE" &&
+						req.Header.Get("X-App-Client-ID") == mockClientId &&
+						req.Header.Get("X-App-Client-Key") == mockClientKey
 				})).Run(func(args mock.Arguments) {
 					for _, c := range mockClient.ExpectedCalls {
 						c.ReturnArguments = mock.Arguments{&http.Response{
@@ -185,23 +108,66 @@ func TestDeleteRequest_deleteBlobberFile(t *testing.T) {
 		},
 		{
 			name: "Test_Delete_Blobber_File_Success",
-			setup: func(t *testing.T, testName string) {
-				mockClient.On("Do", mock.AnythingOfType("*http.Request")).Run(func(args mock.Arguments) {
-					for _, c := range mockClient.ExpectedCalls {
-						c.ReturnArguments = mock.Arguments{&http.Response{
-							StatusCode: http.StatusOK,
-							Body: func() io.ReadCloser {
-								jsonFR, err := json.Marshal(fileref.ReferencePath{
-									Meta: map[string]interface{}{
-										"type": mockType,
-									},
-								})
-								require.NoError(t, err)
-								return ioutil.NopCloser(bytes.NewReader([]byte(jsonFR)))
-							}(),
-						}, nil}
+			parameters: parameters{
+				referencePathToRetrieve: fileref.ReferencePath{
+					Meta: map[string]interface{}{
+						"type": mockType,
+					},
+				},
+				requestFields: map[string]string{
+					"connection_id": mockConnectionId,
+					"path":          mockRemoteFilePath,
+				},
+			},
+			setup: func(t *testing.T, testName string, p parameters) {
+				mockClient.On("Do", mock.MatchedBy(func(req *http.Request) bool {
+					return strings.HasPrefix(req.URL.Path, testName) &&
+						req.Method == "GET" &&
+						req.Header.Get("X-App-Client-ID") == mockClientId &&
+						req.Header.Get("X-App-Client-Key") == mockClientKey
+				})).Return(&http.Response{
+					StatusCode: http.StatusOK,
+					Body: func() io.ReadCloser {
+						jsonFR, err := json.Marshal(p.referencePathToRetrieve)
+						require.NoError(t, err)
+						return ioutil.NopCloser(bytes.NewReader([]byte(jsonFR)))
+					}(),
+				}, nil)
+
+				mockClient.On("Do", mock.MatchedBy(func(req *http.Request) bool {
+					mediaType, params, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
+					require.NoError(t, err)
+					require.True(t, strings.HasPrefix(mediaType, "multipart/"))
+					reader := multipart.NewReader(req.Body, params["boundary"])
+
+					err = nil
+					for {
+						var part *multipart.Part
+						part, err = reader.NextPart()
+						if err != nil {
+							break
+						}
+						expected, ok := p.requestFields[part.FormName()]
+						require.True(t, ok)
+						actual, err := ioutil.ReadAll(part)
+						require.NoError(t, err)
+						require.EqualValues(t, expected, string(actual))
 					}
-				})
+					require.Error(t, err)
+					require.EqualValues(t, "EOF", err.Error())
+
+					return strings.HasPrefix(req.URL.Path, testName) &&
+						req.Method == "DELETE" &&
+						req.Header.Get("X-App-Client-ID") == mockClientId &&
+						req.Header.Get("X-App-Client-Key") == mockClientKey
+				})).Return(&http.Response{
+					StatusCode: http.StatusOK,
+					Body: func() io.ReadCloser {
+						jsonFR, err := json.Marshal(p.referencePathToRetrieve)
+						require.NoError(t, err)
+						return ioutil.NopCloser(bytes.NewReader([]byte(jsonFR)))
+					}(),
+				}, nil)
 			},
 			wantFunc: func(require *require.Assertions, req *DeleteRequest) {
 				require.NotNil(req)
@@ -213,7 +179,7 @@ func TestDeleteRequest_deleteBlobberFile(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			require := require.New(t)
-			tt.setup(t, tt.name)
+			tt.setup(t, tt.name, tt.parameters)
 			req := &DeleteRequest{
 				allocationID:   mockAllocationId,
 				allocationTx:   mockAllocationTxId,
@@ -223,7 +189,7 @@ func TestDeleteRequest_deleteBlobberFile(t *testing.T) {
 					fullconsensus:   4,
 				},
 				ctx:          context.TODO(),
-				connectionID: zboxutil.NewConnectionId(),
+				connectionID: mockConnectionId,
 				wg:           func() *sync.WaitGroup { wg.Add(1); return &wg }(),
 			}
 			req.blobbers = append(req.blobbers, &blockchain.StorageNode{
@@ -250,6 +216,7 @@ func TestDeleteRequest_ProcessDelete(t *testing.T) {
 		mockBlobberId      = "mock blobber id"
 		mockBlobberUrl     = "mockblobberurl"
 		mockType           = "f"
+		mockConnectionId   = "1234567890"
 	)
 
 	var mockClient = mocks.HttpClient{}
@@ -349,7 +316,7 @@ func TestDeleteRequest_ProcessDelete(t *testing.T) {
 		setup       func(*testing.T, string, int, int, DeleteRequest)
 		wantErr     bool
 		errMsg      string
-		wantFunc    func(require *require.Assertions, req *DeleteRequest)
+		wantFunc    func(*require.Assertions, *DeleteRequest)
 	}{
 		{
 			name:        "Test_All_Blobber_Delete_Attribute_Success",
@@ -404,7 +371,7 @@ func TestDeleteRequest_ProcessDelete(t *testing.T) {
 					fullconsensus:   4,
 				},
 				ctx:          context.TODO(),
-				connectionID: zboxutil.NewConnectionId(),
+				connectionID: mockConnectionId,
 			}
 			for i := 0; i < tt.numBlobbers; i++ {
 				req.blobbers = append(req.blobbers, &blockchain.StorageNode{
