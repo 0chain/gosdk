1e9a3679d099340103a7b62c9aa2185ededfcf81
Alex Guo <chessnut@outlook.com>
2021-06-30 03:29:49 -0500

    get InitZCNSDK tested and working in the browser

--- _sdkver/wallet.go
+++ _sdkver/wallet.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	// "fmt"
-	// "strconv"
+	"fmt"
 	"syscall/js"
 	"github.com/0chain/gosdk/zcncore"
 )
 
-// // JS does not have int64 so we must take a string instead of int64.
-// func strToInt64(s string) int64 {
-// 	tokens, err := strconv.ParseInt(s, 10, 64)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return tokens
-// }
-//
-// func int64ToStr(x int64) string {
-// 	return strconv.FormatInt(x, 10)
-// }
-
 func InitZCNSDK(this js.Value, p []js.Value) interface{} {
 	blockWorker := p[0].String()
 	signscheme := p[1].String()
-	err := zcncore.InitZCNSDK(blockWorker, signscheme)
-	if err != nil {
-		return err.Error()
-	}
-	return nil
+
+	handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		resolve := args[0]
+		// reject := args[1]
+
+		go func() {
+			err := zcncore.InitZCNSDK(blockWorker, signscheme)
+			if err != nil {
+				fmt.Println("error:", err)
+			}
+			resolve.Invoke()
+		}()
+
+		return nil
+	})
+
+	// Create and return the Promise object
+	promiseConstructor := js.Global().Get("Promise")
+	return promiseConstructor.New(handler)
 }
