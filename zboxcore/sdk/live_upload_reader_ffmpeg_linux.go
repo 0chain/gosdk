1f7662e1e320c0a216b600644bb488a9bf18bd7f
Lz <imlangzi@qq.com>
2021-07-13 22:08:27 +0800

    feat(upload&download):added m3u8 support in linux

--- zboxcore/sdk/live_upload_reader_ffmpeg_linux.go
+++ zboxcore/sdk/live_upload_reader_ffmpeg_linux.go
@@ -2,10 +2,36 @@ package sdk
 
 import (
 	"strconv"
+	"strings"
 )
 
 // buildFfmpegArgs build ffmpeg arguments for linux
 func buildFfmpegArgs(fileName string, delay int) []string {
+	if strings.HasSuffix(fileName, ".m3u8") {
+		return []string{
+			//	"-thread_queue_size", "50",
+			"-f", "v4l2",
+			"-i", "/dev/video0",
+			"-f", "alsa",
+			"-i", "hw:0",
+			"-preset", "ultrafast",
+			"-tune", "zerolatency",
+			"-vcodec", "libx264",
+			"-r", "30",
+			"-b:v", "512k",
+			"-acodec", "aac",
+			"-strict", "-2",
+			"-ac", "2",
+			"-ab", "32k",
+			"-ar", "44100",
+			"-flags", "+cgop",
+			"-g", "30",
+			"-hls_time", strconv.Itoa(delay),
+			"-loglevel", "warning",
+			fileName, //*.m3u8
+		}
+	}
+
 	return []string{
 		//"-thread_queue_size", "10",
 		"-f", "v4l2",
@@ -25,7 +51,7 @@ func buildFfmpegArgs(fileName string, delay int) []string {
 		"-map", "0",
 		"-map", "1",
 		"-f", "segment",
-		"-segment_time",  strconv.Itoa(delay),
+		"-segment_time", strconv.Itoa(delay),
 		"-loglevel", "warning",
 		fileName,
 	}
