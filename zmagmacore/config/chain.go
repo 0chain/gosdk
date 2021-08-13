3588d86bc68334d72af6b6ae4ca12897d2f6e735
murashovven <murashovven@gmail.com>
2021-08-13 10:55:28 +0300

    Minor refactoring

--- zmagmacore/config/chain.go
+++ zmagmacore/config/chain.go
@@ -1,8 +1,7 @@
 package config
 
 type (
-	// ServerChain represents config options described in "server_chain" section of the config yaml file.
-	// ServerChain must be a field of Config struct
+	// ServerChain represents config describes blockchain options and signature scheme options.
 	ServerChain struct {
 		ID              string `yaml:"id"`
 		OwnerID         string `yaml:"owner_id"`
