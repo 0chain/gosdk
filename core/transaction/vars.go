ba8587553343d51e78cba8c6915f06c1ed36d01f
Lz <imlangzi@qq.com>
2021-08-16 15:47:01 +0800

    fix(transaction):#195 update http reqeust with resty

--- core/transaction/vars.go
+++ core/transaction/vars.go
@@ -17,8 +17,11 @@ func SetConfig(c *conf.Config) {
 }
 
 var (
+	ErrInvalidRequest = errors.New("[txn] invalid request")
+
 	// ErrNoAvailableSharder no any available sharder
 	ErrNoAvailableSharder = errors.New("[txn] there is no any available sharder")
+
 	// ErrNoTxnDetail No transaction detail was found on any of the sharders
 	ErrNoTxnDetail = errors.New("[txn] no transaction detail was found on any of the sharders")
 
