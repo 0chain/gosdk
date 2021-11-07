cd6fab3aebeb9ab8baf6c8c930c9b7d3c97b61dd
amanjain <amanjain5221@gmail.com>
2021-11-07 22:36:19 +0530

    update tests

--- core/clients/blobberClient/client_integration_test.go
+++ core/clients/blobberClient/client_integration_test.go
@@ -1105,18 +1105,18 @@ func TestBlobberClient_IntegrationTest(t *testing.T) {
 		//root, _ := os.Getwd()
 		//path := strings.Split(root, `core`)
 
-		err := os.MkdirAll(`blobber/files/files/exa/mpl/eId/objects/tmp/Mon/Wen`, 0755)
+		err := os.MkdirAll(`/blobber/files/files/exa/mpl/eId/objects/tmp/Mon/Wen`, 0700)
 		if err != nil {
 			t.Fatal(err)
 		}
 		defer func() {
-			err := os.RemoveAll(`blobber/files/files/exa/mpl/eId/objects/tmp/Mon`)
+			err := os.RemoveAll(`/blobber/files/files/exa/mpl/eId/objects/tmp/Mon`)
 			if err != nil {
 				t.Fatal(err)
 			}
 		}()
 
-		f, err := os.Create(`blobber/files/files/exa/mpl/eId/objects/tmp/Mon/Wen/MyFile`)
+		f, err := os.Create(`/blobber/files/files/exa/mpl/eId/objects/tmp/Mon/Wen/MyFile`)
 		if err != nil {
 			t.Fatal(err)
 		}
