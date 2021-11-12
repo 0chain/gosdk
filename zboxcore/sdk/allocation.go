68cee906a40d64cb243a7645c0e63b1e1ff68208
Piers Shepperson <piers.shepperson@btinternet.com>
2021-11-12 16:14:22 +0000

    add is nft to allocation

--- zboxcore/sdk/allocation.go
+++ zboxcore/sdk/allocation.go
@@ -145,6 +145,7 @@ type Allocation struct {
 	Stats          *AllocationStats          `json:"stats"`
 	TimeUnit       time.Duration             `json:"time_unit"`
 	IsImmutable    bool                      `json:"is_immutable"`
+	IsNft          bool                      `json:"is_nft"`
 
 	// BlobberDetails contains real terms used for the allocation.
 	// If the allocation has updated, then terms calculated using
