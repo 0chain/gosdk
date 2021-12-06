3a1b7da53455cf43e73553a7ecc427d670b94dcb
Lz <imlangzi@qq.com>
2021-12-06 21:39:50 +0800

    feat: get wasm sdk worked with js-client-sdk and block-explorer (#309)
    * fix(bls): renamed scheme files
    
    * fix(zcncrypto):#294 extract bls stuff to interface
    
    * fix(zcncrypto):#294 migrated ThresholdSignatureScheme with bls interface
    
    * fix(zcncrypto):#294 fixed nil exception on threshold sginature unmarshal
    
    * fix(zcncrypto):#294 merged ThresholdSignatureScheme into SignuatureScheme
    
    * fix(zcncrypto):#294 added JsObject
    
    * fix(zcncrypto):#294 fixed golangci-lint issues
    
    * feat(wasmsdk):#294 added wasm bridge and got sign worked
    
    * feat(wasmsdk):#294 got wasm sdk initialized and got blobber.delete worked
    
    * feat(wasmsdk):#294 fixed commitToMetadata bug
    
    * feat(wasmsdk):0chain/gosdk#294 migrated rename/copy/move/share
    
    * feat(wasmsdk):0chain/gosdk#294 updated README.md for rename/copy/move/share
    
    * feat(wasmsdk):0chain/gosdk#294 migrated getEncryptedPublicKey
    
    * feat(wasmsdk):0chain/gosdk#294 removed MiraclToHerumiPK
    
    * feat(wasmsdk):0chain/gosdk#294 fixed Array for input binder
    
    * feat(wasmsdk):0chain/gosdk#294 fixed nil reference excepion on list files
    
    * feat(wasmsdk):0chain/gosdk#294 added fireTransactionAdd
    
    * feat(wasmsdk):#294 fixed mock issue for deleteObject
    
    * feat(wasmsdk):#294 fixed bug on bind nil to js.Value

--- core/zcncrypto/bls0chain_test.go
+++ core/zcncrypto/bls0chain_test.go
@@ -1,154 +1,68 @@
+//go:build !js && !wasm
 // +build !js,!wasm
 
 package zcncrypto
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/0chain/errors"
 	"github.com/herumi/bls-go-binary/bls"
 	"github.com/stretchr/testify/require"
-
-	"github.com/0chain/gosdk/core/encryption"
 )
 
-var verifyPublickey = `e8a6cfa7b3076ae7e04764ffdfe341632a136b52953dfafa6926361dd9a466196faecca6f696774bbd64b938ff765dbc837e8766a5e2d8996745b2b94e1beb9e`
-var signPrivatekey = `5e1fc9c03d53a8b9a63030acc2864f0c33dffddb3c276bf2b3c8d739269cc018`
-var data = `TEST`
-var blsWallet *Wallet
+const (
+	testMnemonic = "silent tape impulse glimpse state craft sheriff embody bonus clay confirm column swift kingdom door stove mad switch chalk theory pause canoe insane struggle"
 
-func TestSignatureScheme(t *testing.T) {
-	sigScheme := NewSignatureScheme("bls0chain")
-	switch sigScheme.(type) {
-	case SignatureScheme:
-		// pass
-	default:
-		t.Fatalf("Signature scheme invalid")
-	}
-	w, err := sigScheme.GenerateKeys()
-	if err != nil {
-		t.Fatalf("Generate Key failed %s", errors.Top(err))
-	}
-	if w.ClientID == "" || w.ClientKey == "" || len(w.Keys) != 1 || w.Mnemonic == "" {
-		t.Fatalf("Invalid keys generated")
-	}
-	blsWallet = w
+	testHerumiPublicKey    = "fd2f78b5988719434d6a0782231962934fe1a6f805f98e1bff2c90399a765500ffff9a1cc8c5826feea66d738a7e74ffba7f7dd23e499b5817d8a88e68185f95"
+	testHerumiPublicKeyStr = "1 55769a39902cff1b8ef905f8a6e14f9362192382076a4d43198798b5782ffd 155f18688ea8d817589b493ed27d7fbaff747e8a736da6ee6f82c5c81c9affff e72525d5dda83d7b169653d3a78bd6d6e36cee1f9974d8f30cbfac33a18efb9 19c1c219dbd76990330f778f18d472f10494a6811bb46e36d21bfdf273c03220"
+	testHerumiPrivateKey   = "baa512aee00f5ff9eafcd82a16fa81d450b2a1a1e35f638cb7e4c2caf01bc407"
+
+	testMiraclPublicKeyStr = "55769a39902cff1b8ef905f8a6e14f9362192382076a4d43198798b5782ffd155f18688ea8d817589b493ed27d7fbaff747e8a736da6ee6f82c5c81c9affffe72525d5dda83d7b169653d3a78bd6d6e36cee1f9974d8f30cbfac33a18efb919c1c219dbd76990330f778f18d472f10494a6811bb46e36d21bfdf273c03220"
+)
+
+func TestGenerateKeys(t *testing.T) {
+	herumi := &HerumiScheme{}
+
+	w1, err := herumi.RecoverKeys(testMnemonic)
+
+	require.NoError(t, err)
+
+	require.Equal(t, testHerumiPublicKey, w1.Keys[0].PublicKey)
+
+	var pk1 bls.PublicKey
+	err = pk1.DeserializeHexStr(w1.Keys[0].PublicKey)
+	require.NoError(t, err)
+
+	require.Equal(t, testHerumiPublicKeyStr, pk1.GetHexString())
+
+	require.NoError(t, err)
+
 }
 
-func TestSSSignAndVerify(t *testing.T) {
-	signScheme := NewSignatureScheme("bls0chain")
-	signScheme.SetPrivateKey(signPrivatekey)
+func TestSignAndVerify(t *testing.T) {
+	signScheme := &HerumiScheme{}
+
+	w, err := signScheme.RecoverKeys(testMnemonic)
+
+	var pk = w.Keys[0].PublicKey
+	var pk1 bls.PublicKey
+
+	pk1.DeserializeHexStr(pk)
+
+	fmt.Println(pk1.GetHexString())
+
+	require.NoError(t, err)
+
 	hash := Sha3Sum256(data)
 	signature, err := signScheme.Sign(hash)
 	if err != nil {
 		t.Fatalf("BLS signing failed")
 	}
-	verifyScheme := NewSignatureScheme("bls0chain")
-	verifyScheme.SetPublicKey(verifyPublickey)
+	verifyScheme := &HerumiScheme{}
+	err = verifyScheme.SetPublicKey(w.Keys[0].PublicKey)
+	require.NoError(t, err)
 	if ok, err := verifyScheme.Verify(signature, hash); err != nil || !ok {
 		t.Fatalf("Verification failed\n")
 	}
-}
-
-func BenchmarkBLSSign(b *testing.B) {
-	sigScheme := NewSignatureScheme("bls0chain")
-	sigScheme.SetPrivateKey(signPrivatekey)
-	for i := 0; i < b.N; i++ {
-		_, err := sigScheme.Sign(encryption.Hash(data))
-		if err != nil {
-			b.Fatalf("BLS signing failed")
-		}
-	}
-}
-
-func TestRecoveryKeys(t *testing.T) {
-
-	sigScheme := NewSignatureScheme("bls0chain")
-	TestSignatureScheme(t)
-	w, err := sigScheme.RecoverKeys(blsWallet.Mnemonic)
-	if err != nil {
-		t.Fatalf("set Recover Keys failed")
-	}
-	if w.ClientID != blsWallet.ClientID || w.ClientKey != blsWallet.ClientKey {
-		t.Fatalf("Recover key didn't match with generated keys")
-	}
-
-}
-
-func TestCombinedSignAndVerify(t *testing.T) {
-	sk0 := `c36f2f92b673cf057a32e8bd0ca88888e7ace40337b737e9c7459fdc4c521918`
-	sk1 := `704b6f489583bf1118432fcfb38e63fc2d4b61e524fb196cbd95413f8eb91c12`
-	primaryKey := `f72fd53ee85e84157d3106053754594f697e0bfca1f73f91a41f7bb0797d901acefd80fcc2da98aae690af0ee9c795d6590c1808f26490306433b4e9c42f7b1f`
-
-	hash := Sha3Sum256(data)
-	// Create signatue for 1
-	sig0 := NewSignatureScheme("bls0chain")
-	err := sig0.SetPrivateKey(sk0)
-	if err != nil {
-		t.Fatalf("Set private key failed - %s", errors.Top(err))
-	}
-	signature, err := sig0.Sign(hash)
-	if err != nil {
-		t.Fatalf("BLS signing failed")
-	}
-	// Create signature for second
-	sig1 := NewSignatureScheme("bls0chain")
-	err = sig1.SetPrivateKey(sk1)
-	if err != nil {
-		t.Fatalf("Set private key failed - %s", errors.Top(err))
-	}
-	addSig, err := sig1.Add(signature, hash)
-
-	verifyScheme := NewSignatureScheme("bls0chain")
-	err = verifyScheme.SetPublicKey(primaryKey)
-	if err != nil {
-		t.Fatalf("Set public key failed")
-	}
-	if ok, err := verifyScheme.Verify(addSig, hash); err != nil || !ok {
-		t.Fatalf("Verification failed\n")
-	}
-}
-
-func TestSplitKey(t *testing.T) {
-	primaryKeyStr := `c36f2f92b673cf057a32e8bd0ca88888e7ace40337b737e9c7459fdc4c521918`
-	sig0 := NewSignatureScheme("bls0chain")
-	err := sig0.SetPrivateKey(primaryKeyStr)
-	if err != nil {
-		t.Fatalf("Set private key failed - %s", errors.Top(err))
-	}
-	hash := Sha3Sum256(data)
-	signature, err := sig0.Sign(hash)
-	if err != nil {
-		t.Fatalf("BLS signing failed")
-	}
-	numSplitKeys := int(2)
-	w, err := sig0.SplitKeys(numSplitKeys)
-	if err != nil {
-		t.Fatalf("Splitkeys key failed - %s", errors.Top(err))
-	}
-	sigAggScheme := make([]SignatureScheme, numSplitKeys)
-	for i := 0; i < numSplitKeys; i++ {
-		sigAggScheme[i] = NewSignatureScheme("bls0chain")
-		sigAggScheme[i].SetPrivateKey(w.Keys[i].PrivateKey)
-	}
-	var aggrSig string
-	for i := 1; i < numSplitKeys; i++ {
-		tmpSig, _ := sigAggScheme[i].Sign(hash)
-		aggrSig, _ = sigAggScheme[0].Add(tmpSig, hash)
-	}
-	if aggrSig != signature {
-		t.Fatalf("split key signature failed")
-	}
-}
-
-func TestMiraclToHerumiPK(t *testing.T) {
-	miraclpk1 := `0418a02c6bd223ae0dfda1d2f9a3c81726ab436ce5e9d17c531ff0a385a13a0b491bdfed3a85690775ee35c61678957aaba7b1a1899438829f1dc94248d87ed36817f6dfafec19bfa87bf791a4d694f43fec227ae6f5a867490e30328cac05eaff039ac7dfc3364e851ebd2631ea6f1685609fc66d50223cc696cb59ff2fee47ac`
-	pk1 := MiraclToHerumiPK(miraclpk1)
-
-	require.EqualValues(t, pk1, "68d37ed84842c91d9f82389489a1b1a7ab7a957816c635ee750769853aeddf1b490b3aa185a3f01f537cd1e9e56c43ab2617c8a3f9d2a1fd0dae23d26b2ca018")
-
-	// Assert DeserializeHexStr works on the output of MiraclToHerumiPK
-	var pk bls.PublicKey
-	err := pk.DeserializeHexStr(pk1)
-	require.NoError(t, err)
 }
---
+++ wasmsdk/README.md
@@ -0,0 +1,22 @@
+# 0chain/wasmsdk
+The 0chain wasm SDK is written in Go programming language, and released with WebAssembly binary format 
+
+*NOTE* please use `try{await zcn.sdk.[method];}catch(err){...}` to handle any error from wasm sdk in js
+
+## ZCN methods
+
+- `zcn.sdk.init(chainID, blockWorker, signatureScheme string, minConfirmation, minSubmit, confirmationChainLength int)`: init wasm sdk 
+- `zcn.bls.setWallet(bls,clientID, sk, pk string)`: set bls.SecretKey on js, and call `zcn.sdk.setWallet` to set wallet on go.
+- `zcn.sdk.setWallet(clientID,publicKey string)`: set wallet on go
+- `zcn.sdk.getEncryptedPublicKey(mnemonic string)`: get encrypted public key by mnemonic
+
+
+## Blobber methods
+- `zcn.sdk.delete(allocationID, remotePath string,commit bool)`:    delete remote file from blobbers
+- `zcn.sdk.rename(allocationID, remotePath, destName string, commit bool)`: rename a file existing already on dStorage. Only the allocation's owner can rename a file.
+- `zcn.sdk.copy(allocationID, remotePath, destPath string, commit bool)`:   copy file to another folder path on blobbers
+- `zcn.sdk.move(allocationID, remotePath, destPath string, commit bool)`:   move file to another remote folder path on dStorage. Only the owner of the allocation can copy an object.
+- `zcn.sdk.share(allocationID, remotePath, clientID, encryptionPublicKey string, expiration int, revoke bool)`:    generate an authtoken that provides authorization to the holder to the specified file on the remotepath.
+
+
+  
