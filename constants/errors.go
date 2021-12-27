0bb2770cc847e3228248a9da30b6dcc80972972c
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2021-12-27 13:47:32 +0545

    Add error codes and define errors with it

--- constants/errors.go
+++ constants/errors.go
@@ -1,7 +1,11 @@
 // Package constants provides constants.the convention of naming is to use MixedCaps or mixedCaps rather than underscores to write multiword names. https://golang.org/doc/effective_go#mixed-caps
 package constants
 
-import "errors"
+import (
+	"errors"
+
+	zerrors "github.com/0chain/errors"
+)
 
 var (
 	// ErrInvalidParameter parameter is not specified or invalid
@@ -15,4 +19,59 @@ var (
 
 	// ErrNotImplemented feature/method is not implemented yet
 	ErrNotImplemented = errors.New("Not Implemented")
+)
+
+// zerrors code
+const (
+	UnknownCodeError             = ""
+	TooManyRequestsError         = "too_many_requests"
+	ParseError                   = "parse_error"
+	ValidateError                = "validate_error"
+	TransactionVerificationError = "verify_transaction"
+	TransactionSendError         = "transaction_send_error"
+	LeafExistError               = "leaf_exist"
+	RecoverKeysError             = "recover_keys"
+	WasmNotSupportedError        = "wasm_not_supported"
+	WasmNotInitializedError      = "wasm_not_initialized"
+	KeysGenerationError          = "generate_keys"
+	WalletMarshalError           = "wallet_marshal"
+	InvalidReferencePathError    = "invalid_reference_path"
+	InvalidListPathError         = "invalid_list_path"
+	FileNotFoundError            = "file_not_found"
+	InvalidInputLengthError      = "invalid_input_length"
+	InvalidRefPathError          = "invalid_ref_path"
+	WriteMarkerValidationError   = "write_marker_validation_failed"
+	SdkNotInitializedError       = "sdk_not_initialized"
+	InvalidPathError             = "invalid_path"
+	InvalidNameError             = "invalid_name"
+	AuthTicketDecodeError        = "auth_ticket_decode_error"
+	ConsensusNotReachedError     = "consensus_not_reached"
+	ConsensusFailedError         = "consensus_failed"
+)
+
+var (
+	ErrUnknown                 = zerrors.New(UnknownCodeError, "")
+	ErrTooManyRequests         = zerrors.New(TooManyRequestsError, "")
+	ErrParse                   = zerrors.New(ParseError, "")
+	ErrValidate                = zerrors.New(ValidateError, "")
+	ErrTransactionVerification = zerrors.New(TransactionVerificationError, "")
+	ErrTransactionSend         = zerrors.New(TransactionSendError, "")
+	ErrLeafExist               = zerrors.New(LeafExistError, "")
+	ErrRecoverKeys             = zerrors.New(RecoverKeysError, "")
+	ErrWasmNotSupported        = zerrors.New(WasmNotSupportedError, "")
+	ErrWasmNotInitialized      = zerrors.New(WasmNotInitializedError, "")
+	ErrKeysGeneration          = zerrors.New(KeysGenerationError, "")
+	ErrWalletMarshal           = zerrors.New(WalletMarshalError, "")
+	ErrInvalidReferencePath    = zerrors.New(InvalidReferencePathError, "")
+	ErrInvalidListPath         = zerrors.New(InvalidListPathError, "")
+	ErrFileNotFound            = zerrors.New(FileNotFoundError, "")
+	ErrInvalidInputLength      = zerrors.New(InvalidInputLengthError, "")
+	ErrInvalidRefPath          = zerrors.New(InvalidRefPathError, "")
+	ErrWriteMarkerValidation   = zerrors.New(WriteMarkerValidationError, "")
+	ErrSdkNotInitialized       = zerrors.New(SdkNotInitializedError, "")
+	ErrInvalidPath             = zerrors.New(InvalidPathError, "")
+	ErrInvalidName             = zerrors.New(InvalidNameError, "")
+	ErrAuthTicketDecode        = zerrors.New(AuthTicketDecodeError, "")
+	ErrConsensusNotReached     = zerrors.New(ConsensusNotReachedError, "")
+	ErrConsensusFailed         = zerrors.New(ConsensusFailedError, "")
 )
