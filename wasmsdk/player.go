9a01eb66152bedb0cb83a85a011dd9b64b7a008f
Lz <imlangzi@qq.com>
2021-12-31 08:26:40 +0800

    fix(wasm): added player

--- wasmsdk/player.go
+++ wasmsdk/player.go
@@ -4,12 +4,27 @@
 package main
 
 import (
-	"fmt"
+	"github.com/0chain/gosdk/zboxcore/marker"
 	"github.com/0chain/gosdk/zboxcore/sdk"
-	"net/http"
 )
 
-func Play(allocationID, remotePath, authTicket, lookupHash string) error {
+type Player struct {
+	allocationID string
+	remotePath   string
+	authTicket   string
+	lookupHash   string
+
+	isOwner       bool
+	allocationObj *sdk.Allocation
+	authTicketObj *marker.AuthTicket
+}
+
+func (p *Player) getList() {
+
+}
+
+func CreatePalyer(allocationID, remotePath, authTicket, lookupHash string) error {
+	player := &Player{}
 
 	//player is owner
 	if len(remotePath) > 0 {
@@ -23,9 +38,48 @@ func Play(allocationID, remotePath, authTicket, lookupHash string) error {
 			return err
 		}
 
+		player.isOwner = true
+		player.allocationObj = allocationObj
+
+		// ref, err := allocationObj.ListDir(remotePath)
+		// if err != nil {
+		// 	return err
+		// }
+
+		// ref.Children, nil
+
 	}
 
 	//player is viewer via shared authticket
+
+	at, err := sdk.InitAuthTicket(authTicket).Unmarshall()
+
+	if err != nil {
+		PrintError(err)
+		return err
+	}
+
+	allocationObj, err := sdk.GetAllocationFromAuthTicket(authTicket)
+	if err != nil {
+		PrintError("Error fetching the allocation", err)
+		return err
+	}
+
+	player.isOwner = false
+	player.allocationObj = allocationObj
+	player.authTicketObj = at
+
+	// //get list from authticket
+	// ref, err := allocationObj.ListDirFromAuthTicket(authTicket, lookupHash)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	return nil
+
+}
+
+func Play(allocationID, remotePath, authTicket, lookupHash string) error {
 
 	return nil
 }
