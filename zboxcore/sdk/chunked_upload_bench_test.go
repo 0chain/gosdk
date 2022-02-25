c9cb535013221a63c479b04198038afafc4a0d3e
Lz <imlangzi@qq.com>
2022-02-25 22:44:37 +0800

    feat(writemarker): added ShouldWork test

--- zboxcore/sdk/chunked_upload_bench_test.go
+++ zboxcore/sdk/chunked_upload_bench_test.go
@@ -47,7 +47,7 @@ func BenchmarkChunkedUpload(b *testing.B) {
 
 	logger.Logger.SetLevel(2)
 
-	server := dev.NewBlobberServer()
+	server := dev.NewBlobberServer(nil)
 	defer server.Close()
 
 	benchmarks := []struct {
--- zboxcore/sdk/writemarker_mutex_test.go
+++ zboxcore/sdk/writemarker_mutex_test.go
@@ -1,7 +1,55 @@
 package sdk
 
-import "testing"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"testing"
 
-func TestWriteMarkerMutext(t *testing.T) {
+	"github.com/0chain/gosdk/dev"
+	"github.com/0chain/gosdk/dev/mock"
+	"github.com/0chain/gosdk/sdks/blobber"
+	"github.com/0chain/gosdk/zboxcore/blockchain"
+	"github.com/0chain/gosdk/zboxcore/zboxutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWriteMarkerMutext_Should_Work(t *testing.T) {
+	a := &Allocation{
+		ID:           "TestAllocation_UpdateFile",
+		Tx:           "TestAllocation_UpdateFile",
+		DataShards:   2,
+		ParityShards: 1,
+	}
+	setupMockAllocation(t, a)
+
+	require := require.New(t)
+
+	resp := &WMLockResult{
+		Status: WMLockStatusOK,
+	}
+
+	respBuf, _ := json.Marshal(resp)
+	m := make(mock.ResponseMap)
+	m[http.MethodPost+":"+blobber.EndpointWriteMarkerLock+a.Tx] = mock.Response{
+		StatusCode: http.StatusOK,
+		Body:       respBuf,
+	}
+
+	server := dev.NewBlobberServer(m)
+	defer server.Close()
+
+	for i := 0; i < 3; i++ {
+		a.Blobbers = append(a.Blobbers, &blockchain.StorageNode{
+			ID:      "write_marker_mutex_" + strconv.Itoa(i),
+			Baseurl: server.URL,
+		})
+	}
+
+	mutex := CreateWriteMarkerMutex(a)
+
+	err := mutex.Lock(context.TODO(), zboxutil.NewConnectionId())
+	require.Nil(err)
 
 }
