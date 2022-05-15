2c88b834d077261b0558835a6a13294820b2b0b4
DmitryD <dima.dyachkov@gmail.com>
2022-05-16 03:24:18 +0800

    clean

--- znft/ethereum_wallet.go
+++ znft/ethereum_wallet.go
@@ -2,7 +2,6 @@ package znft
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"math/big"
 	"path"
 	"time"
@@ -23,51 +22,6 @@ func (b *Configuration) CreateEthClient() (*ethclient.Client, error) {
 		Logger.Error(err)
 	}
 	return client, err
-}
-
-//  _allowances[owner][spender] = amount;
-// as a spender, ERC20 WZCN token must increase allowance for the bridge to make burn on behalf of WZCN owner
-
-func CreateSignedTransaction(
-	chainID *big.Int,
-	client *ethclient.Client,
-	fromAddress common.Address,
-	privateKey *ecdsa.PrivateKey,
-	gasLimitUnits uint64,
-) *bind.TransactOpts {
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		Logger.Fatal(err)
-	}
-
-	// eth_estimateGas
-	// EstimateGas tries to estimate the gas needed to execute a specific transaction based on
-	// the current pending state of the backend blockchain. There is no guarantee that this is
-	// the true gas limit requirement as other transactions may be added or removed by miners,
-	// but it should provide a basis for setting a reasonable default.
-
-	// eth_gasPrice
-	// retrieves the currently suggested gas price to allow a timely
-	// execution of a transaction
-
-	gasPriceWei, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		Logger.Fatal(err)
-	}
-
-	opts, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-	if err != nil {
-		Logger.Fatal(err)
-	}
-
-	valueWei := new(big.Int).Mul(big.NewInt(0), big.NewInt(params.Wei))
-
-	opts.Nonce = big.NewInt(int64(nonce))
-	opts.Value = valueWei         // in wei
-	opts.GasLimit = gasLimitUnits // in units
-	opts.GasPrice = gasPriceWei   // wei
-
-	return opts
 }
 
 func (b *Configuration) CreateSignedTransactionFromKeyStore(ctx context.Context, gasLimitUnits uint64) *bind.TransactOpts {
