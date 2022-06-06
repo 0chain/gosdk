75d70128627b7705643dcd51eb45fa2beeaa9e59
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-06-06 13:26:21 +0545

    Fix error

--- wasmsdk/blobber.go
+++ wasmsdk/blobber.go
@@ -344,15 +344,15 @@ func downloadFile(allocationObj *sdk.Allocation, authTicket string, authTicketOb
 
 		if downloadThumbnailOnly {
 			errE = allocationObj.DownloadThumbnailFromAuthTicket(localPath,
-				authTicket, lookupHash, fileName,  statusBar)
+				authTicket, lookupHash, fileName, statusBar)
 		} else {
 			if startBlock != 0 || endBlock != 0 {
 				errE = allocationObj.DownloadFromAuthTicketByBlocks(
 					localPath, authTicket, int64(startBlock), int64(endBlock), blocksPerMarker,
-					lookupHash, fileName,  statusBar)
+					lookupHash, fileName, statusBar)
 			} else {
 				errE = allocationObj.DownloadFromAuthTicket(localPath,
-					authTicket, lookupHash, fileName,  statusBar)
+					authTicket, lookupHash, fileName, statusBar)
 			}
 		}
 	} else if len(remotePath) > 0 {
@@ -386,7 +386,7 @@ func downloadFile(allocationObj *sdk.Allocation, authTicket string, authTicketOb
 }
 
 // Download download file
-func Download(allocationID, remotePath, authTicket, lookupHash string, downloadThumbnailOnly,  autoCommit bool) (*DownloadCommandResponse, error) {
+func Download(allocationID, remotePath, authTicket, lookupHash string, downloadThumbnailOnly, autoCommit bool) (*DownloadCommandResponse, error) {
 
 	if len(remotePath) == 0 && len(authTicket) == 0 {
 		return nil, RequiredArg("remotePath/authTicket")
@@ -401,7 +401,7 @@ func Download(allocationID, remotePath, authTicket, lookupHash string, downloadT
 
 	downloader, err := sdk.CreateDownloader(allocationID, localPath, remotePath,
 		sdk.WithAuthticket(authTicket, lookupHash),
-		sdk.WithOnlyThumbnail(downloadThumbnailOnly),
+		sdk.WithOnlyThumbnail(downloadThumbnailOnly))
 
 	if err != nil {
 		PrintError(err.Error())
