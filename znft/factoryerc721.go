a8698816a2b36a4dec280b821c7fd41fa56f219d
DmitryD <dima.dyachkov@gmail.com>
2022-06-12 23:05:51 +0800

    Added tampletes for factories

---
+++ znft/factoryerc721.go
@@ -0,0 +1,32 @@
+package znft
+
+import (
+	"context"
+	"math/big"
+
+	factory "github.com/0chain/gosdk/znft/contracts/factory/binding"
+)
+
+// Solidity functions
+
+// function create(
+//	address module,
+//	string calldata name,
+//	string calldata symbol,
+//	string calldata uri,
+//	uint256 max,
+//	uint256 price,
+//	uint256 batch,
+//	bytes calldata data) external returns (address) {
+
+// function register(address module, bool status)
+
+type IFactory interface {
+	Create(module, name, symbol, uri string, max, price, batch *big.Int, data string) string
+	Register(address string, status bool)
+}
+
+type Factory struct {
+	session *factory.BindingSession
+	ctx     context.Context
+}
