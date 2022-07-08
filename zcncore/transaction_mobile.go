2271b49c76382c36408a12566e8defe8af21d2ac
peterlimg <peterlimg@protonmail.com>
2022-07-08 21:23:38 +1000

    Parse empty string for transaction fee/value as 0

--- zcncore/transaction_mobile.go
+++ zcncore/transaction_mobile.go
@@ -320,9 +320,13 @@ func (im *inputMap) AddField(key, value string) {
 }
 
 func parseCoinStr(vs string) (uint64, error) {
+	if vs == "" {
+		return 0, nil
+	}
+
 	v, err := strconv.ParseUint(vs, 10, 64)
 	if err != nil {
-		return 0, fmt.Errorf("invalid token value: %v", vs)
+		return 0, fmt.Errorf("invalid token value: %v, err: %v", vs, err)
 	}
 
 	return v, nil
