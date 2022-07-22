d072bdf047e0695f151f14e27930e234c40047ca
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-07-22 20:18:19 +0545

    Use common function

--- zboxcore/allocationchange/newfile.go
+++ zboxcore/allocationchange/newfile.go
@@ -4,6 +4,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/0chain/gosdk/core/common"
 	"github.com/0chain/gosdk/zboxcore/fileref"
 )
 
@@ -13,7 +14,11 @@ type NewFileChange struct {
 }
 
 func (ch *NewFileChange) ProcessChange(rootRef *fileref.Ref) error {
-	tSubDirs := getSubDirs(filepath.Dir(ch.File.Path))
+	tSubDirs, err := common.GetPathFields(filepath.Dir(ch.File.Path))
+	if err != nil {
+		return err
+	}
+
 	dirRef := rootRef
 	for i := 0; i < len(tSubDirs); i++ {
 		found := false
