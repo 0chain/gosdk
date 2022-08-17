a81b0833112a3513184dd4c9b6eebf094b066598
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-08-17 19:43:22 +0545

    Replace client id with allocation id. Remove redundant initialization of readcounter

--- zboxcore/sdk/read_counter_ctr.go
+++ zboxcore/sdk/read_counter_ctr.go
@@ -9,35 +9,28 @@ type blobberReadCounter struct {
 	mu *sync.RWMutex
 }
 
-var brc *blobberReadCounter
-
-func InitReadCounter() {
-	if brc == nil {
-		brc = &blobberReadCounter{
-			m:  make(map[string]int64),
-			mu: &sync.RWMutex{},
-		}
-	}
-
+var brc = &blobberReadCounter{
+	m:  make(map[string]int64),
+	mu: &sync.RWMutex{},
 }
 
-func setBlobberReadCtr(clientID, blobberID string, ctr int64) {
-	key := clientID + blobberID
+func setBlobberReadCtr(allocID, blobberID string, ctr int64) {
+	key := allocID + blobberID
 	brc.mu.Lock()
 	brc.m[key] = ctr
 	brc.mu.Unlock()
 }
 
-func getBlobberReadCtr(clientID, blobberID string) int64 {
-	key := clientID + blobberID
+func getBlobberReadCtr(allocID, blobberID string) int64 {
+	key := allocID + blobberID
 	brc.mu.RLock()
 	c := brc.m[key]
 	brc.mu.RUnlock()
 	return c
 }
 
-func incBlobberReadCtr(clientID, blobberID string, numBlocks int64) {
-	key := clientID + blobberID
+func incBlobberReadCtr(allocID, blobberID string, numBlocks int64) {
+	key := allocID + blobberID
 	brc.mu.Lock()
 	brc.m[key] += numBlocks
 	brc.mu.Unlock()
