f6b87f63dfffb9c5657a91b2cbb82cd80d171d07
dabasov <yuderbasov@gmail.com>
2022-08-28 16:55:43 +0300

    added bridge client initialization

--- wasmsdk/sdk.go
+++ wasmsdk/sdk.go
@@ -39,7 +39,7 @@ func initSDKs(chainID, blockWorker, signatureScheme string,
 	return nil
 }
 
-func SetWallet(clientID, publicKey string) {
+func SetWallet(clientID, publicKey, privateKey string) {
 	c := client.GetClient()
 	c.ClientID = clientID
 	c.ClientKey = publicKey
@@ -47,6 +47,21 @@ func SetWallet(clientID, publicKey string) {
 	if len(zboxHost) > 0 {
 		zboxClient = zbox.NewClient(zboxHost, c.ClientID, c.ClientKey)
 	}
+
+	//w.Version = cw.Version
+	//w.DateCreated = cw.Version
+
+	w := &zcncrypto.Wallet{
+		ClientID:  clientID,
+		ClientKey: publicKey,
+		Keys: []zcncrypto.KeyPair{
+			{
+				PrivateKey: privateKey,
+				PublicKey:  publicKey,
+			},
+		},
+	}
+	zcncore.SetWallet(*w, false)
 }
 
 func GetEncryptedPublicKey(mnemonic string) (string, error) {
