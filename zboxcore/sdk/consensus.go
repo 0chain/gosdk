68777ef256ed3b60be10dd48308dfca7b87dd55f
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-08-30 19:27:41 +0545

    Use field instead of struct composition

--- zboxcore/sdk/consensus.go
+++ zboxcore/sdk/consensus.go
@@ -3,7 +3,7 @@ package sdk
 import "sync"
 
 type Consensus struct {
-	sync.RWMutex
+	mu              *sync.RWMutex
 	consensus       int // Total successful and valid response from blobbers
 	consensusThresh int // Consensus threshold percentage
 	fullconsensus   int // Total number of blobbers in allocation
@@ -11,34 +11,34 @@ type Consensus struct {
 
 // Done increase consensus by 1
 func (c *Consensus) Done() {
-	c.Lock()
+	c.mu.Lock()
 	c.consensus++
-	c.Unlock()
+	c.mu.Unlock()
 }
 
 // Reset reset consensus to 0
 func (c *Consensus) Reset() {
-	c.Lock()
+	c.mu.Lock()
 	c.consensus = 0
-	c.Unlock()
+	c.mu.Unlock()
 }
 
 func (c *Consensus) Init(threshConsensus, fullConsensus int) {
-	c.Lock()
+	c.mu.Lock()
 	c.consensusThresh = threshConsensus
 	c.fullconsensus = fullConsensus
-	c.Unlock()
+	c.mu.Unlock()
 }
 
 func (c *Consensus) getConsensus() int {
-	c.RLock()
-	defer c.RUnlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	return c.consensus
 }
 
 func (c *Consensus) isConsensusOk() bool {
-	c.RLock()
-	defer c.RUnlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 
 	return c.consensus >= c.consensusThresh
 }
