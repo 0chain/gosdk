3abcd90e2c80f27a1504c715a0b57e251eba1739
Lz <imlangzi@qq.com>
2022-10-28 16:02:55 +0800

    fix(mobile): exposed GetPublicEncryptionKey

--- wasmsdk/sdk.go
+++ wasmsdk/sdk.go
@@ -12,7 +12,6 @@ import (
 	"github.com/0chain/gosdk/core/zcncrypto"
 	"github.com/0chain/gosdk/wasmsdk/zbox"
 	"github.com/0chain/gosdk/zboxcore/client"
-	"github.com/0chain/gosdk/zboxcore/encryption"
 	"github.com/0chain/gosdk/zboxcore/sdk"
 	"github.com/0chain/gosdk/zcncore"
 )
