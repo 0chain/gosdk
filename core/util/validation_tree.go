91190184355bdacaa32d5d79953fdfd788a1b016
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2023-01-09 09:55:32 +0545

    Add verify function, add comments Replace sha256 with sha3.N256

--- core/util/validation_tree.go
+++ core/util/validation_tree.go
@@ -1,12 +1,20 @@
 package util
 
 import (
-	"crypto/sha256"
+	"bytes"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"hash"
 	"math"
 	"sync"
+
+	"golang.org/x/crypto/sha3"
+)
+
+const (
+	Left = iota
+	Right
 )
 
 type ValidationTree struct {
@@ -23,6 +31,14 @@ type ValidationTree struct {
 
 func (v *ValidationTree) GetLeaves() [][]byte {
 	return v.leaves
+}
+
+func (v *ValidationTree) SetLeaves(leaves [][]byte) {
+	v.leaves = leaves
+}
+
+func (v *ValidationTree) GetDataSize() int64 {
+	return v.dataSize
 }
 
 func (v *ValidationTree) GetValidationRoot() []byte {
@@ -71,7 +87,7 @@ func (v *ValidationTree) calculateRoot() {
 	depth := v.CalculateDepth()
 	nodes := make([][]byte, totalLeaves)
 	copy(nodes, v.leaves)
-	h := sha256.New()
+	h := sha3.New256()
 
 	for i := 0; i < depth; i++ {
 		if len(nodes) == 1 {
@@ -120,9 +136,144 @@ func (v *ValidationTree) Finalize() error {
 }
 
 func NewValidationTree(dataSize int64) *ValidationTree {
-	v := ValidationTree{
+	totalLeaves := (dataSize + MaxMerkleLeavesSize - 1) / MaxMerkleLeavesSize
+
+	return &ValidationTree{
 		writeLock: &sync.Mutex{},
 		dataSize:  dataSize,
+		h:         sha3.New256(),
+		leaves:    make([][]byte, totalLeaves),
 	}
-	return &v
+}
+
+// MerklePathForMultiLeafVerification is used to verify multiple blocks with single instance of
+// merkle path. Usually client would request with counter incremented by 10. So if the block size
+// is 64KB and counter is incremented by 10 then client is requesting 640 KB of data. Blobber can then
+// provide sinlge merkle path instead of sending 10 merkle paths.
+type MerklePathForMultiLeafVerification struct {
+	// RootHash that was signed by the client
+	RootHash []byte
+	// Nodes contains a slice for each merkle node level. Each slice contains hash that will
+	// be concatenated with the calculated hash from the level below.
+	// It is used together with field Index [][]int
+	// Length of Nodes will be according to number of blocks requested. If whole data is requested then
+	// blobber will send nil for Nodes i.e. length of Nodes will become zero.
+	Nodes [][][]byte `json:"nodes"`
+	// Index slice that determines whether to concatenate hash to left or right.
+	// It should have maximum of length 2 and minimum of 0. It is used together with field Nodes [][][]byte
+	Index [][]int `json:"index"`
+	// DataSize is size of data received by the blobber for the respective file.
+	// It is not same as actual file size
+	DataSize      int64
+	totalLeaves   int
+	requiredDepth int
+}
+
+/*
+VerifyMultipleBlocks will verify merkle path for continuous data which is multiple of 64KB blocks
+
+There can be at most 2 hashes in the input i.e. of the format below:
+h1, data1, data2, data3, data4, h2
+Note that data1, data2, data3,... should be continuous data
+
+
+i#3                h14
+i#2       h12             h13
+i#1    h7      h8      h9    h10
+i#0  h0, h1, h2, h3, h4, h5, h6
+
+i#5                                                  h37
+i#4                                 h35                                       h36
+i#3                h32                               h33                      h34
+i#2        h27             h28             h29                 h30            h31
+i#1    h18     h19     h20     h21     h22      h23       h24       h25,      h26
+i#0  h0, h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, h14, h15, h16, h17
+
+Consider there are 16 leaves(0..15) with total data = 16*64KB.
+If client wants data from 3-10 then blobber needs to provide:
+1. Two nodes from i#0, [h2, h11]
+2. One node from i#1, [h16]
+3. One node from i#2, [h27]
+
+If client had required data from 2-9 then blobber would have to provide:
+1. Zero nodes from i#0
+2. Two nodes from i#1, [h16, h21]
+3. One node from i#2, [h27]
+*/
+func (m *MerklePathForMultiLeafVerification) VerifyMultipleBlocks(data []byte) error {
+
+	hashes := make([][]byte, 0)
+	h := sha3.New256()
+	for i := 0; i < len(data); i += MaxMerkleLeavesSize {
+		endIndex := i + MaxMerkleLeavesSize
+		if endIndex > len(data) {
+			endIndex = len(data)
+		}
+		h.Reset()
+		h.Write(data[i:endIndex])
+		hashes = append(hashes, h.Sum(nil))
+	}
+
+	if m.requiredDepth == 0 {
+		m.calculateRequiredLevels()
+	}
+	for i := 0; i < m.requiredDepth-1; i++ {
+		if len(m.Nodes) > i {
+			if len(m.Index[i]) == 2 {
+				hashes = append([][]byte{m.Nodes[i][0]}, hashes...)
+				hashes = append(hashes, m.Nodes[i][1])
+			} else if len(m.Index[i]) == 1 {
+				if m.Index[i][0] == Right {
+					hashes = append(hashes, m.Nodes[i][0])
+				} else {
+					hashes = append([][]byte{m.Nodes[i][0]}, hashes...)
+				}
+			}
+		}
+
+		hashes = m.calculateIntermediateHashes(hashes)
+
+	}
+
+	if !bytes.Equal(m.RootHash, hashes[0]) {
+		return fmt.Errorf("calculated root %s; expected %s",
+			hex.EncodeToString(hashes[0]),
+			hex.EncodeToString(m.RootHash))
+	}
+	return nil
+}
+
+func (m *MerklePathForMultiLeafVerification) calculateIntermediateHashes(hashes [][]byte) [][]byte {
+	newHashes := make([][]byte, 0)
+	h := sha3.New256()
+	if len(hashes)%2 == 0 {
+		for i := 0; i < len(hashes); i += 2 {
+			h.Reset()
+			h.Write(hashes[i])
+			h.Write(hashes[i+1])
+			newHashes = append(newHashes, h.Sum(nil))
+		}
+	} else {
+		for i := 0; i < len(hashes)-1; i += 2 {
+			h.Reset()
+			h.Write(hashes[i])
+			h.Write(hashes[i+1])
+			newHashes = append(newHashes, h.Sum(nil))
+		}
+		h.Reset()
+		h.Write(hashes[len(hashes)-1])
+		newHashes = append(newHashes, h.Sum(nil))
+	}
+	return newHashes
+}
+
+func (m *MerklePathForMultiLeafVerification) calculateTotalLeaves() {
+	m.totalLeaves = int((m.DataSize + MaxMerkleLeavesSize - 1) / MaxMerkleLeavesSize)
+}
+
+func (m *MerklePathForMultiLeafVerification) calculateRequiredLevels() {
+	if m.totalLeaves == 0 {
+		m.calculateTotalLeaves()
+	}
+	m.requiredDepth = int(math.Ceil(math.Log2(float64(m.totalLeaves)))) + 1 // Add root hash to be a level
 }
