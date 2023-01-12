313eb8b70b1ceb0919ec5fc8ccb46dc4d7e19f3b
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2023-01-12 15:54:05 +0545

    Remove redundant capacity

--- zboxcore/sdk/chunked_upload_chunk_reader.go
+++ zboxcore/sdk/chunked_upload_chunk_reader.go
@@ -139,7 +139,7 @@ func (r *chunkedUploadChunkReader) Next() (*ChunkData, error) {
 		FragmentSize: 0,
 	}
 
-	chunkBytes := make([]byte, r.chunkDataSizePerRead, r.chunkDataSizePerRead*2)
+	chunkBytes := make([]byte, r.chunkDataSizePerRead)
 	readLen, err := r.fileReader.Read(chunkBytes)
 
 	if err != nil {
