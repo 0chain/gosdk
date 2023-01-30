df784ea3edd249e5a8bb058a3df91b218b59fc78
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2023-01-30 08:51:27 +0545

    Use context cancel

--- zboxcore/sdk/allocation.go
+++ zboxcore/sdk/allocation.go
@@ -279,7 +279,6 @@ func (a *Allocation) CreateDir(remotePath string) error {
 		mu:           &sync.Mutex{},
 		dirMask:      zboxutil.NewUint128(1).Lsh(uint64(len(a.Blobbers))).Sub64(1),
 		connectionID: zboxutil.NewConnectionId(),
-		ctx:          a.ctx,
 		remotePath:   remotePath,
 		wg:           &sync.WaitGroup{},
 		Consensus: Consensus{
@@ -287,6 +286,7 @@ func (a *Allocation) CreateDir(remotePath string) error {
 			fullconsensus:   a.fullconsensus,
 		},
 	}
+	req.ctx, req.ctxCncl = context.WithCancel(a.ctx)
 
 	err := req.ProcessDir(a)
 	return err
@@ -773,7 +773,7 @@ func (a *Allocation) deleteFile(path string, threshConsensus, fullConsensus int)
 	req.allocationID = a.ID
 	req.allocationTx = a.Tx
 	req.consensus.Init(threshConsensus, fullConsensus)
-	req.ctx = a.ctx
+	req.ctx, req.ctxCncl = context.WithCancel(a.ctx)
 	req.remotefilepath = path
 	req.connectionID = zboxutil.NewConnectionId()
 	req.deleteMask = zboxutil.NewUint128(1).Lsh(uint64(len(a.Blobbers))).Sub64(1)
@@ -814,7 +814,7 @@ func (a *Allocation) RenameObject(path string, destName string) error {
 	req.newName = destName
 	req.consensus.fullconsensus = a.fullconsensus
 	req.consensus.consensusThresh = a.consensusThreshold
-	req.ctx = a.ctx
+	req.ctx, req.ctxCncl = context.WithCancel(a.ctx)
 	req.remotefilepath = path
 	req.renameMask = zboxutil.NewUint128(1).Lsh(uint64(len(a.Blobbers))).Sub64(1)
 	req.maskMU = &sync.Mutex{}
@@ -852,7 +852,7 @@ func (a *Allocation) MoveObject(srcPath string, destPath string) error {
 	req.destPath = destPath
 	req.fullconsensus = a.fullconsensus
 	req.consensusThresh = a.consensusThreshold
-	req.ctx = a.ctx
+	req.ctx, req.ctxCncl = context.WithCancel(a.ctx)
 	req.remotefilepath = srcPath
 	req.moveMask = zboxutil.NewUint128(1).Lsh(uint64(len(a.Blobbers))).Sub64(1)
 	req.maskMU = &sync.Mutex{}
@@ -890,7 +890,7 @@ func (a *Allocation) CopyObject(path string, destPath string) error {
 	req.destPath = destPath
 	req.fullconsensus = a.fullconsensus
 	req.consensusThresh = a.consensusThreshold
-	req.ctx = a.ctx
+	req.ctx, req.ctxCncl = context.WithCancel(a.ctx)
 	req.remotefilepath = path
 	req.copyMask = zboxutil.NewUint128(1).Lsh(uint64(len(a.Blobbers))).Sub64(1)
 	req.maskMU = &sync.Mutex{}
--- zboxcore/sdk/chunked_upload.go
+++ zboxcore/sdk/chunked_upload.go
@@ -154,9 +154,9 @@ func CreateChunkedUpload(
 		uploadTimeOut: DefaultUploadTimeOut,
 		commitTimeOut: DefaultUploadTimeOut,
 		maskMu:        &sync.Mutex{},
-		ctx:           allocationObj.ctx,
 		opCode:        opCode,
 	}
+	su.ctx, su.ctxCncl = context.WithCancel(allocationObj.ctx)
 
 	if isUpdate {
 		su.httpMethod = http.MethodPut
@@ -326,6 +326,7 @@ type ChunkedUpload struct {
 	commitTimeOut time.Duration
 	maskMu        *sync.Mutex
 	ctx           context.Context
+	ctxCncl       context.CancelFunc
 }
 
 // progressID build local progress id with [allocationid]_[Hash(LocalPath+"_"+RemotePath)]_[RemoteName] format
@@ -410,6 +411,7 @@ func (su *ChunkedUpload) createEncscheme() encryption.EncryptionScheme {
 
 // Start start/resume upload
 func (su *ChunkedUpload) Start() error {
+	defer su.ctxCncl()
 
 	if su.statusCallback != nil {
 		su.statusCallback.Started(su.allocationObj.ID, su.fileMeta.RemotePath, su.opCode, int(su.fileMeta.ActualSize)+int(su.fileMeta.ActualThumbnailSize))
@@ -489,15 +491,15 @@ func (su *ChunkedUpload) Start() error {
 		blobbers, &su.consensus, 0, su.uploadTimeOut,
 		su.progress.ConnectionID)
 
-	defer su.writeMarkerMutex.Unlock(
-		su.ctx, su.uploadMask, blobbers, su.uploadTimeOut, su.progress.ConnectionID) //nolint: errcheck
-
 	if err != nil {
 		if su.statusCallback != nil {
 			su.statusCallback.Error(su.allocationObj.ID, su.fileMeta.Path, su.opCode, err)
 		}
 		return err
 	}
+
+	defer su.writeMarkerMutex.Unlock(
+		su.ctx, su.uploadMask, blobbers, su.uploadTimeOut, su.progress.ConnectionID) //nolint: errcheck
 
 	return su.processCommit()
 }
