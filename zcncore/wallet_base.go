7510cb953e99fc89ca3c7be78ab56bcc9ccc96e3
Anas Nisar <55254721+manasnisar@users.noreply.github.com>
2023-02-09 18:43:22 +0500

    adding function to replicate user aggregates from sharders (#747)
    Co-authored-by: Yury <yuderbasov@gmail.com>
    Co-authored-by: Lz <imlangzi@qq.com>

--- zcncore/wallet_base.go
+++ zcncore/wallet_base.go
@@ -88,6 +88,7 @@ const (
 	STORAGE_GET_SHARDER_SNAPSHOT    = STORAGESC_PFX + "/replicate-sharder-aggregates"
 	STORAGE_GET_AUTHORIZER_SNAPSHOT = STORAGESC_PFX + "/replicate-authorizer-aggregates"
 	STORAGE_GET_VALIDATOR_SNAPSHOT  = STORAGESC_PFX + "/replicate-validator-aggregates"
+	STORAGE_GET_USER_SNAPSHOT       = STORAGESC_PFX + "/replicate-user-aggregates"
 )
 
 const (
@@ -160,6 +161,7 @@ const (
 	OpStorageSCGetSharderSnapshots
 	OpStorageSCGetAuthorizerSnapshots
 	OpStorageSCGetValidatorSnapshots
+	OpStorageSCGetUserSnapshots
 	OpZCNSCGetGlobalConfig
 	OpZCNSCGetAuthorizer
 	OpZCNSCGetAuthorizerNodes
@@ -1113,6 +1115,18 @@ func GetAuthorizerSnapshots(offset int64, cb GetInfoCallback) (err error) {
 		"offset": strconv.FormatInt(offset, 10),
 	})
 	go GetInfoFromAnySharder(url, OpStorageSCGetAuthorizerSnapshots, cb)
+	return
+}
+
+// GetUserSnapshots replicates user aggregates from events_db.
+func GetUserSnapshots(offset int64, cb GetInfoCallback) (err error) {
+	if err = CheckConfig(); err != nil {
+		return
+	}
+	var url = withParams(STORAGE_GET_USER_SNAPSHOT, Params{
+		"offset": strconv.FormatInt(offset, 10),
+	})
+	go GetInfoFromAnySharder(url, OpStorageSCGetUserSnapshots, cb)
 	return
 }
 
