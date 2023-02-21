a6e6546e5b052f932676d2f535b7d41c19bbd66d
pgrudra <pgrudrakshi@gmail.com>
2023-02-21 15:34:00 +0530

    added GetUserLockedTotal to mobilesdk

---
+++ mobilesdk/zcn/transaction_query.go
@@ -0,0 +1,12 @@
+package zcn
+
+import (
+	"github.com/0chain/gosdk/zcncore"
+)
+
+// GetUserLockedTotal get total token user locked
+// # Inputs
+//   - clientID wallet id
+func GetUserLockedTotal(clientID string) (int64, error) {
+	return zcncore.GetUserLockedTotal(clientID)
+}
