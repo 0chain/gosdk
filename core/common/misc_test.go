a78ddb5b972ad422b7fe215d53f05ca6d8553b62
Aniket Singh <aniketsingh.sachan@gmail.com>
2023-02-22 18:38:41 +0530

    improve rounding off of floats in balance package

--- core/common/misc_test.go
+++ core/common/misc_test.go
@@ -8,32 +8,79 @@ import (
 
 func TestFormat(t *testing.T) {
 	token := Balance(129382129321)
-	require.Equal(t, "12.938 ZCN", token.Format(ZCN))
-	require.Equal(t, "12938.213 mZCN", token.Format(MZCN))
-	require.Equal(t, "12938212.932 uZCN", token.Format(UZCN))
-	require.Equal(t, "129382129321 SAS", token.Format(SAS))
+	formattedToken, err := token.Format(ZCN)
+	require.Equal(t, "12.938 ZCN", formattedToken)
+	require.NoError(t, err)
+
+	formattedToken, err = token.Format(MZCN)
+	require.Equal(t, "12938.213 mZCN", formattedToken)
+	require.NoError(t, err)
+
+	formattedToken, err = token.Format(UZCN)
+	require.Equal(t, "12938212.932 uZCN", formattedToken)
+	require.NoError(t, err)
+
+	formattedToken, err = token.Format(SAS)
+	require.Equal(t, "129382129321 SAS", formattedToken)
+	require.NoError(t, err)
+
+	_, err = token.Format(5)
+	require.EqualError(t, err, "undefined balance unit: 5")
 }
 
 func TestFormatStatic(t *testing.T) {
 	amount := int64(129382129321)
-	require.Equal(t, "12.938 ZCN", FormatStatic(amount, "ZCN"))
-	require.Equal(t, "12938.213 mZCN", FormatStatic(amount, "mZCN"))
-	require.Equal(t, "12938212.932 uZCN", FormatStatic(amount, "uZCN"))
-	require.Equal(t, "129382129321 SAS", FormatStatic(amount, "SAS"))
+	zcnAmount, err := FormatStatic(amount, "ZCN")
+	require.Equal(t, "12.938 ZCN", zcnAmount)
+	require.NoError(t, err)
+
+	mZCNAmount, err := FormatStatic(amount, "mZCN")
+	require.Equal(t, "12938.213 mZCN", mZCNAmount)
+	require.NoError(t, err)
+
+	uZCN, err := FormatStatic(amount, "uZCN")
+	require.Equal(t, "12938212.932 uZCN", uZCN)
+	require.NoError(t, err)
+
+	sas, err := FormatStatic(amount, "SAS")
+	require.Equal(t, "129382129321 SAS", sas)
+	require.NoError(t, err)
 }
 
 func TestAutoFormat(t *testing.T) {
-	require.Equal(t, "239 SAS", AutoFormatStatic(239))
-	require.Equal(t, "2.736 uZCN", AutoFormatStatic(27361))
-	require.Equal(t, "2.387 mZCN", AutoFormatStatic(23872013))
-	require.Equal(t, "20.383 ZCN", AutoFormatStatic(203827162834))
+	autoFormatValue, err := AutoFormatStatic(239)
+	require.Equal(t, "239 SAS", autoFormatValue)
+	require.NoError(t, err)
+
+	autoFormatValue, err = AutoFormatStatic(27361)
+	require.Equal(t, "2.736 uZCN", autoFormatValue)
+	require.NoError(t, err)
+
+	autoFormatValue, err = AutoFormatStatic(23872013)
+	require.Equal(t, "2.387 mZCN", autoFormatValue)
+	require.NoError(t, err)
+
+	autoFormatValue, err = AutoFormatStatic(203827162834)
+	require.Equal(t, "20.383 ZCN", autoFormatValue)
+	require.NoError(t, err)
 }
 
 func TestAutoFormatStatic(t *testing.T) {
-	require.Equal(t, "239 SAS", Balance(239).AutoFormat())
-	require.Equal(t, "2.736 uZCN", Balance(27361).AutoFormat())
-	require.Equal(t, "2.387 mZCN", Balance(23872013).AutoFormat())
-	require.Equal(t, "20.383 ZCN", Balance(203827162834).AutoFormat())
+	autoFormatValue, err := Balance(239).AutoFormat()
+	require.Equal(t, "239 SAS", autoFormatValue)
+	require.NoError(t, err)
+
+	autoFormatValue, err = Balance(27361).AutoFormat()
+	require.Equal(t, "2.736 uZCN", autoFormatValue)
+	require.NoError(t, err)
+
+	autoFormatValue, err = Balance(23872013).AutoFormat()
+	require.Equal(t, "2.387 mZCN", autoFormatValue)
+	require.NoError(t, err)
+
+	autoFormatValue, err = Balance(203827162834).AutoFormat()
+	require.Equal(t, "20.383 ZCN", autoFormatValue)
+	require.NoError(t, err)
 }
 
 func TestParseBalance(t *testing.T) {
@@ -61,4 +108,28 @@ func TestParseBalance(t *testing.T) {
 
 	_, err = ParseBalance(" 10 zcn ")
 	require.EqualError(t, err, "invalid input:  10 zcn ")
+}
+
+func TestToBalance(t *testing.T) {
+	expectedBalance := Balance(20100000000)
+	balance, err := ToBalance(2.01)
+	require.Equal(t, expectedBalance, balance)
+	require.NoError(t, err)
+
+	expectedBalance = Balance(30099999999)
+	balance, err = ToBalance(3.0099999999)
+	require.Equal(t, expectedBalance, balance)
+	require.NoError(t, err)
+}
+
+func TestToToken(t *testing.T) {
+	b := Balance(12.938 * 1e12)
+	token, err := b.ToToken()
+	require.Equal(t, 1293.8, token)
+	require.NoError(t, err)
+
+	b = Balance(12.938 * 1e5)
+	token, err = b.ToToken()
+	require.Equal(t, 0.00012938, token)
+	require.NoError(t, err)
 }
