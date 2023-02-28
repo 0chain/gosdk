7607bee435ae47c9a7befb096781d494d87e0ce6
Piers Shepperson <piers.shepperson@btinternet.com>
2023-02-28 15:21:48 +0000

    remove unneeded parameter

--- zboxcore/sdk/sdk.go
+++ zboxcore/sdk/sdk.go
@@ -1255,7 +1255,7 @@ func KillProvider(providerId string, providerType ProviderType) (string, int64, 
 	return hash, n, err
 }
 
-func ShutdownProvider(providerId string, providerType ProviderType) (string, int64, error) {
+func ShutdownProvider(providerType ProviderType) (string, int64, error) {
 	if !sdkInitialized {
 		return "", 0, sdkNotInitialized
 	}
