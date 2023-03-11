4c4396e28263872ea09db0d793dd0d48f67b74f5
YarikRevich <yariksvitlitskiy81@gmail.com>
2023-03-11 17:36:11 -0500

    fix: fixed bugs

--- zcnbridge/zcnsc/burn_ticket.go
+++ zcnbridge/zcnsc/burn_ticket.go
@@ -3,5 +3,5 @@ package zcnsc
 // BurnTicket represents WZCN burn ticket details
 type BurnTicket struct {
 	TransactionHash string `json:"transactionHash"`
-	Nonce           int64  `json:"nonce"`
+	Nonce           string `json:"nonce"`
 }
