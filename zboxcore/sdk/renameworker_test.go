13f37376bf66ce7e10891863e3772b15fb4749c7
Manish-210 <210manishprajapati@gmail.com>
2023-03-15 11:47:52 +0530

    old blobber rename error

--- zboxcore/sdk/renameworker_test.go
+++ zboxcore/sdk/renameworker_test.go
@@ -415,7 +415,7 @@ func TestRenameRequest_ProcessRename(t *testing.T) {
 			numCorrect:  2,
 			setup:       setupHttpResponses,
 			wantErr:     true,
-			errMsg:      "rename_failed",
+			errMsg:      "consensus_not_met",
 		},
 		{
 			name:        "Test_All_Blobber_Error_On_Rename_Failure",
