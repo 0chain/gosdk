9a2849fdd0729483fae4300b7f41ed2575b77ecf
Manish-210 <210manishprajapati@gmail.com>
2023-03-17 17:06:59 +0530

    changing consensus sign

--- zboxcore/sdk/renameworker.go
+++ zboxcore/sdk/renameworker.go
@@ -187,7 +187,7 @@ func (req *RenameRequest) ProcessRename() error {
 			wgErrorsList = append(wgErrorsList, err)
 	}
 
-	if req.consensus.consensusThresh<req.consensus.getConsensus() && len(wgErrorsList) >=1 {
+	if req.consensus.consensusThresh>req.consensus.getConsensus() && len(wgErrorsList) >=1 {
 		return errors.New("rename_failed", fmt.Sprintf("Rename failed. %s", wgErrorsList[0]))
 	}
 
