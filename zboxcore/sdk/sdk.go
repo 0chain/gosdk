31a87a0c556a3323cf6877e5dd50d5fc2a546ff1
dabasov <yuderbasov@gmail.com>
2023-04-13 18:13:26 +0300

    fixed min-lock-demand

--- zboxcore/sdk/sdk.go
+++ zboxcore/sdk/sdk.go
@@ -6,11 +6,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
 
+	"github.com/0chain/common/core/currency"
 	"github.com/0chain/errors"
 	"github.com/0chain/gosdk/core/conf"
 	"github.com/0chain/gosdk/core/logger"
@@ -1465,36 +1467,44 @@ func CommitToFabric(metaTxnData, fabricConfigJSON string) (string, error) {
 	return fabricResponse, err
 }
 
+// expire in milliseconds
 func GetAllocationMinLock(
 	datashards, parityshards int,
 	size, expiry int64,
 	readPrice, writePrice PriceRange,
 ) (int64, error) {
-	preferredBlobberIds, err := GetBlobberIds(blockchain.GetPreferredBlobbers())
+	baSize := int64(math.Ceil(float64(size) / float64(datashards)))
+	totalSize := baSize * int64(datashards+parityshards)
+	config, err := GetStorageSCConfig()
 	if err != nil {
-		return -1, errors.New("failed_get_blobber_ids", "failed to get preferred blobber ids: "+err.Error())
+		return 0, err
+	}
+	t := config.Fields["time_unit"]
+	timeunitStr, ok := t.(string)
+	if !ok {
+		return 0, fmt.Errorf("bad time_unit type")
+	}
+	timeunit, err := time.ParseDuration(timeunitStr)
+	if err != nil {
+		return 0, fmt.Errorf("bad time_unit format")
 	}
 
-	allocationRequestData, err := getNewAllocationBlobbers(
-		datashards, parityshards, size, expiry, readPrice, writePrice, preferredBlobberIds)
-	if err != nil {
-		return -1, errors.New("failed_get_allocation_blobbers", "failed to get blobbers for allocation: "+err.Error())
-	}
-	allocationRequest, _ := json.Marshal(allocationRequestData)
-	params := make(map[string]string)
-	params["allocation_data"] = string(allocationRequest)
-
-	allocationsBytes, err := zboxutil.MakeSCRestAPICall(STORAGE_SCADDRESS, "/allocation_min_lock", params, nil)
-	if err != nil {
-		return 0, errors.New("allocation_min_lock_fetch_error", "Error fetching the allocation min lock."+err.Error())
+	duration := expiry / timeunit.Milliseconds()
+	if expiry%timeunit.Milliseconds() != 0 {
+		duration++
 	}
 
-	var response = make(map[string]int64)
-	err = json.Unmarshal(allocationsBytes, &response)
+	sizeInGB := float64(totalSize) / GB
+	cost := float64(duration) * (sizeInGB*float64(writePrice.Max) + sizeInGB*float64(readPrice.Max))
+	coin, err := currency.Float64ToCoin(cost)
 	if err != nil {
-		return 0, errors.New("allocation_min_lock_decode_error", "Error decoding the response."+err.Error())
+		return 0, err
 	}
-	return response["min_lock_demand"], nil
+	i, err := coin.Int64()
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
 }
 
 // calculateAllocationFileOptions calculates the FileOptions 16-bit mask given the user input
