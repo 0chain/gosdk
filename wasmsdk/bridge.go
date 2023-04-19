8194be56637d17108fc54a3dba803d1c6da64b45
YarikRevich <yariksvitlitskiy2@gmail.com>
2023-04-19 14:23:23 +0200

    fix: removed direct subgraph calls

--- wasmsdk/bridge.go
+++ wasmsdk/bridge.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"strconv"
 	"time"
 
 	"github.com/0chain/gosdk/zcnbridge"
@@ -23,7 +24,6 @@ import (
 var bridge *zcnbridge.BridgeClient
 
 func initBridge(
-	blockWorker string,
 	ethereumAddress string,
 	bridgeAddress string,
 	authorizersAddress string,
@@ -57,7 +57,6 @@ func initBridge(
 		GasLimit:           gasLimit,
 		Value:              value,
 		ConsensusThreshold: consensusThreshold,
-		BlockWorker:        blockWorker,
 	}
 
 	wallet := zcncore.GetWalletRaw()
@@ -118,30 +117,30 @@ func getMintWZCNPayload(burnTrxHash string) string {
 	return string(result)
 }
 
-// Returns all not processed WZCN burn tickets burned for client id given as a param
-func getNotProcessedWZCNBurnTickets() string {
+// Returns all not processed WZCN burn events for the given client id param
+func getNotProcessedWZCNBurnEvents() string {
 	var mintNonce int64
 	cb := wallet.NewZCNStatus(&mintNonce)
 
 	cb.Begin()
 
 	if err := zcncore.GetMintNonce(cb); err != nil {
-		return errors.Wrap("getNotProcessedWZCNBurnTickets", "failed to retreive last ZCN processed mint nonce", err).Error()
+		return errors.Wrap("getNotProcessedWZCNBurnEvents", "failed to retreive last ZCN processed mint nonce", err).Error()
 	}
 
 	if err := cb.Wait(); err != nil {
-		return errors.Wrap("getNotProcessedWZCNBurnTickets", "failed to retreive last ZCN processed mint nonce", err).Error()
+		return errors.Wrap("getNotProcessedWZCNBurnEvents", "failed to retreive last ZCN processed mint nonce", err).Error()
 	}
 
-	burnTickets, err := bridge.GetNotProcessedWZCNBurnTickets(context.Background(), mintNonce)
+	burnEvents, err := bridge.QueryEthereumBurnEvents(strconv.Itoa(int(mintNonce)))
 	if err != nil {
-		return errors.Wrap("getNotProcessedWZCNBurnTickets", "failed to retreive WZCN burn tickets", err).Error()
+		return errors.Wrap("getNotProcessedWZCNBurnEvents", "failed to retreive WZCN burn events", err).Error()
 	}
 
 	var result []byte
-	result, err = json.Marshal(burnTickets)
+	result, err = json.Marshal(burnEvents)
 	if err != nil {
-		return errors.Wrap("getNotProcessedZCNBurnTickets", "failed to marshal WZCN burn tickets", err).Error()
+		return errors.Wrap("getNotProcessedWZCNBurnEvents", "failed to marshal WZCN burn events", err).Error()
 	}
 
 	return string(result)
--- zcnbridge/config.go
+++ zcnbridge/config.go
@@ -45,14 +45,9 @@ type EthereumConfig struct {
 	Value int64
 }
 
-type SubgraphConfig struct {
-	BlockWorker string
-}
-
 type BridgeClientConfig struct {
 	ContractsRegistry
 	EthereumConfig
-	SubgraphConfig
 	EthereumAddress string
 	Password        string
 	Homedir         string
@@ -124,9 +119,6 @@ func CreateBridgeOwner(bridgeCfg, chainCfg *viper.Viper, walletFile ...string) *
 				GasLimit:        bridgeCfg.GetUint64(fmt.Sprintf("%s.GasLimit", OwnerConfigKeyName)),
 				Value:           bridgeCfg.GetInt64(fmt.Sprintf("%s.Value", OwnerConfigKeyName)),
 			},
-			SubgraphConfig: SubgraphConfig{
-				BlockWorker: chainCfg.GetString("block_worker"),
-			},
 			EthereumAddress: bridgeCfg.GetString(fmt.Sprintf("%s.EthereumAddress", OwnerConfigKeyName)),
 			Password:        bridgeCfg.GetString(fmt.Sprintf("%s.Password", OwnerConfigKeyName)),
 			Homedir:         homedir,
@@ -167,9 +159,6 @@ func CreateBridgeClient(bridgeCfg, chainCfg *viper.Viper, walletFile ...string) 
 				GasLimit:        bridgeCfg.GetUint64(fmt.Sprintf("%s.GasLimit", ClientConfigKeyName)),
 				Value:           bridgeCfg.GetInt64(fmt.Sprintf("%s.Value", ClientConfigKeyName)),
 			},
-			SubgraphConfig: SubgraphConfig{
-				BlockWorker: chainCfg.GetString("block_worker"),
-			},
 			EthereumAddress: bridgeCfg.GetString(fmt.Sprintf("%s.EthereumAddress", ClientConfigKeyName)),
 			Password:        bridgeCfg.GetString(fmt.Sprintf("%s.Password", ClientConfigKeyName)),
 			Homedir:         homedir,
@@ -194,7 +183,6 @@ type BridgeClientYaml struct {
 	GasLimit           uint64
 	Value              int64
 	ConsensusThreshold float64
-	BlockWorker        string
 }
 
 func CreateBridgeClientWithConfig(cfg BridgeClientYaml, wallet *zcncrypto.Wallet) *BridgeClient {
@@ -209,9 +197,6 @@ func CreateBridgeClientWithConfig(cfg BridgeClientYaml, wallet *zcncrypto.Wallet
 				EthereumNodeURL: cfg.EthereumNodeURL,
 				GasLimit:        cfg.GasLimit,
 				Value:           cfg.Value,
-			},
-			SubgraphConfig: SubgraphConfig{
-				BlockWorker: cfg.BlockWorker,
 			},
 			EthereumAddress: cfg.EthereumAddress,
 			Password:        cfg.Password,
