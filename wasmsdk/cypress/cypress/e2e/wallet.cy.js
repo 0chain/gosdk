ae2afe6c5c225c855b80673de2c0d8d79e7398e8
Lz <imlangzi@qq.com>
2023-04-25 18:12:04 +0800

    feat(wasm): added wasm tests

--- wasmsdk/cypress/cypress/e2e/wallet.cy.js
+++ wasmsdk/cypress/cypress/e2e/wallet.cy.js
@@ -1,40 +1,5 @@
 describe('wallet',   () => {
-  it('Create wallet',async () => {
-    cy.intercept('POST', 'http://127.0.0.1:8080/wallet').as('createWallet');
-    cy.intercept('GET', 'https://dev.zus.network/dns/network').as('getDNS');
-
-    cy.visit('http://127.0.0.1:8080')
-    cy.wait('@getDNS').its('response.statusCode').should('eq', 200)
-
-    cy.window().then(async win=>{
-      const resp = await win.fetch('http://127.0.0.1:8080/wallet', {
-        method: 'POST',
-      })
-
-      const w = await resp.json()
-      const wallet = {} 
-      wallet.client_id = w.client_id
-      wallet.mnemonics = w.mnemonics
-      w.keys.forEach(it=>{
-        wallet.public_key = it.public_key
-        wallet.private_key = it.private_key
-      })
-
-      cy.get('#clientId').invoke('val', wallet.client_id)
-      cy.get('#publicKey').invoke('val', wallet.public_key)
-      cy.get('#privateKey').invoke('val', wallet.private_key)
-      cy.get('#mnemonic').invoke('val',wallet.mnemonics)
-    })
-
-
-    cy.wait('@createWallet').its('response.statusCode').should('eq', 200)
-    cy.wait(1500)
-
-    cy.get('#btnShowLogs').click()
-    cy.get('#btnSetWallet').click()
-    cy.wait(1500)
-    cy.get("#btnSendMeTokens").click()
-
+  it('Create wallet',() => {
+    cy.createWallet()
   })
-
 })
--- wasmsdk/cypress/cypress/support/commands.js
+++ wasmsdk/cypress/cypress/support/commands.js
@@ -9,17 +9,65 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('createWallet', () => { 
+  cy.intercept('POST', 'http://127.0.0.1:8080/wallet').as('createWallet');
+  cy.intercept('GET', 'https://dev.zus.network/dns/network').as('getDNS');
+  cy.intercept('GET','**/v1/block/get?round=*').as('faucet')
+  cy.intercept('GET','**/v1/client/get/balance?client_id=*').as("getBalance")
+
+  cy.visit('http://127.0.0.1:8080')
+  cy.wait('@getDNS').its('response.statusCode').should('eq', 200)
+
+  cy.wait(1500)
+
+  cy.get('#btnShowLogs').click()
+
+  cy.window().then(async win=>{
+    const resp = await win.fetch('http://127.0.0.1:8080/wallet', {
+      method: 'POST',
+    })
+
+    const w = await resp.json()
+    const wallet = {} 
+    wallet.client_id = w.client_id
+    wallet.mnemonics = w.mnemonics
+    w.keys.forEach(it=>{
+      wallet.public_key = it.public_key
+      wallet.private_key = it.private_key
+    })
+
+    cy.get('#clientId').invoke('val', wallet.client_id)
+    cy.get('#publicKey').invoke('val', wallet.public_key)
+    cy.get('#privateKey').invoke('val', wallet.private_key)
+    cy.get('#mnemonic').invoke('val',wallet.mnemonics)
+    cy.get('#btnSetWallet').click()
+  })
+
+
+  cy.wait('@createWallet').its('response.statusCode').should('eq', 200)
+  cy.wait(1500)
+
+  cy.get("#btnSendMeTokens").click()
+  cy.wait("@faucet", {timeout:60*1000}).its('response.statusCode').should('eq', 200)
+
+
+  cy.get("#btnGetBalance").click()
+
+  const waitGetBalance = ()=> {
+    cy.wait("@getBalance").then(it=>{
+      if (it.response.statusCode == 200) {
+        expect(it.response.body.balance).to.equal(100000000000)
+      }else{
+        waitGetBalance()
+      }
+    })
+  }
+
+  waitGetBalance()
+ })
+
+ Cypress.Commands.add('createAllocation', () => { 
+
+ })
