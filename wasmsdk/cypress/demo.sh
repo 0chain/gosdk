6ac63545d025569f806b9c1f6d2032c79ddda481
Lz <imlangzi@qq.com>
2023-04-26 17:09:52 +0800

    feat(wasm): added wasm tests

--- wasmsdk/cypress/demo.sh
+++ wasmsdk/cypress/demo.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-if [ -z "$ZCN_WASM_TESTS" ] 
+if [ -z "$RUNNER_NUMBER" ] 
 then
   echo "WASM: SKIP DUE TO NOT SYSTEM-TESTS"
   exit 0
 fi
 
-
 echo "======================================================"
-echo "SETTING WASM TESTS:"
+echo "STARTING WASM DEMO SERVER:"
 echo "======================================================"
-git clone -q https://github.com/0chain/gosdk.git
-cd gosdk
-
-if [[ -n "$ZCN_WASM_GOSDK" && "$ZCN_WASM_GOSDK" != "NONE" ]];
-then
-  git checkout $ZCN_WASM_GOSDK
-fi
-
-CGO_ENABLED=0 GOOS=js GOARCH=wasm go build -o ./wasmsdk/demo/zcn.wasm  ./wasmsdk
 
 HTTPCODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:8080)
 if test $HTTPCODE -eq 200; then
     echo "WASM: shutdown staled demo server"
     curl --silent http://127.0.0.1:8080/shutdown
 fi
-cd ./wasmsdk/demo && go build -o demo .
+echo $(pwd)
+cd ../demo 
 ./demo &
 sleep 3
-cd ../../..
+cd ../
 
 HTTPCODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:8080)
 if test $HTTPCODE -eq 200; then
