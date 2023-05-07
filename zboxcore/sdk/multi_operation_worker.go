11751446d226cdef4264b41799f3bb3975b5cc67
Roshan-Mehta <roshankr.kumar222@gmail.com>
2023-05-08 00:15:15 +0530

    Create connectionObj before sending process request

--- zboxcore/sdk/multi_operation_worker.go
+++ zboxcore/sdk/multi_operation_worker.go
@@ -1,22 +1,30 @@
 package sdk
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
 	"sync"
 	"time"
 
 	"github.com/0chain/errors"
-	// "github.com/0chain/gosdk/constants"
 
 	"github.com/0chain/gosdk/core/util"
 	"github.com/0chain/gosdk/zboxcore/allocationchange"
 	"github.com/0chain/gosdk/zboxcore/client"
 	"github.com/0chain/gosdk/zboxcore/fileref"
+	"github.com/0chain/gosdk/zboxcore/logger"
 	l "github.com/0chain/gosdk/zboxcore/logger"
 
 	"github.com/0chain/gosdk/zboxcore/zboxutil"
 	"github.com/google/uuid"
+)
+
+const (
+	DefaultCreateConnectionTimeOut = 2 * time.Minute
 )
 
 type Operationer interface {
@@ -40,6 +48,98 @@ type MultiOperation struct {
 	changes [][]allocationchange.AllocationChange
 }
 
+func (mo *MultiOperation) createConnectionObj(blobberIdx int) (err error) {
+
+	defer func() {
+		if err != nil {
+			mo.maskMU.Lock()
+			mo.operationMask = mo.operationMask.And(zboxutil.NewUint128(1).Lsh(uint64(blobberIdx)).Not())
+			mo.maskMU.Unlock()
+		}
+	}()
+
+	var (
+		resp           *http.Response
+		shouldContinue bool
+		latestRespMsg  string
+
+		latestStatusCode int
+	)
+	blobber := mo.allocationObj.Blobbers[blobberIdx]
+
+	for i := 0; i < 3; i++ {
+		err, shouldContinue = func() (err error, shouldContinue bool) {
+			body := new(bytes.Buffer)
+			formWriter := multipart.NewWriter(body)
+
+			formWriter.WriteField("connection_id", mo.connectionID)
+			formWriter.Close()
+
+			var httpreq *http.Request
+			httpreq, err = zboxutil.NewConnectionRequest(blobber.Baseurl, mo.allocationObj.Tx, body)
+			if err != nil {
+				l.Logger.Error(blobber.Baseurl, "Error creating rename request", err)
+				return
+			}
+
+			httpreq.Header.Add("Content-Type", formWriter.FormDataContentType())
+			ctx, cncl := context.WithTimeout(mo.ctx, DefaultCreateConnectionTimeOut)
+			defer cncl()
+			resp, err = zboxutil.Client.Do(httpreq.WithContext(ctx))
+
+			if err != nil {
+				logger.Logger.Error("Create Connection: ", err)
+				return
+			}
+
+			if resp.Body != nil {
+				defer resp.Body.Close()
+			}
+			var respBody []byte
+			respBody, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				logger.Logger.Error("Error: Resp ", err)
+				return
+			}
+
+			latestRespMsg = string(respBody)
+			latestStatusCode = resp.StatusCode
+			if resp.StatusCode == http.StatusOK {
+				l.Logger.Info(blobber.Baseurl, " connection obj created.")
+				return
+			}
+
+			if resp.StatusCode == http.StatusTooManyRequests {
+				logger.Logger.Error("Got too many request error")
+				var r int
+				r, err = zboxutil.GetRateLimitValue(resp)
+				if err != nil {
+					logger.Logger.Error(err)
+					return
+				}
+				time.Sleep(time.Duration(r) * time.Second)
+				shouldContinue = true
+				return
+			}
+			l.Logger.Error(blobber.Baseurl, "Response: ", string(respBody))
+			err = errors.New("response_error", string(respBody))
+			return
+		}()
+
+		if err != nil {
+			return
+		}
+		if shouldContinue {
+			continue
+		}
+		return
+	}
+
+	err = errors.New("unknown_issue",
+		fmt.Sprintf("last status code: %d, last response message: %s", latestStatusCode, latestRespMsg))
+	return
+}
+
 func (mo *MultiOperation) Process() error {
 	l.Logger.Info("MultiOperation Process start")
 	wg := &sync.WaitGroup{}
@@ -47,25 +147,29 @@ func (mo *MultiOperation) Process() error {
 	ctx := mo.ctx
 	ctxCncl := mo.ctxCncl
 	defer ctxCncl()
+	// Create connection obj in each blobber
+	for blobberIdx := range mo.allocationObj.Blobbers {
+		wg.Add(1)
+		go func(pos int) {
+			defer wg.Done()
+			err := mo.createConnectionObj(pos)
+			if err != nil {
+				l.Logger.Error(err.Error())
+			}
+		}(blobberIdx)
+	}
+	wg.Wait()
+	// Check consensus
+	if mo.operationMask.CountOnes() < mo.consensusThresh {
+		return errors.New("consensus_not_met",
+			fmt.Sprintf("Multioperation failed. Required consensus %d got %d",
+				mo.consensusThresh, mo.operationMask.CountOnes()))
+	}
+
 	errs := make(chan error, 1)
+
 	for idx, op := range mo.operations {
 		uid := util.GetNewUUID()
-		// Don't use goroutine for the first operation because:
-		// 1. In blobber code we try to fetch the allocation
-		// from the postgress and sharders, if not found blobber try to create it. This is done without lock, so if we
-		// sent multiple goroutine together, blobber will try to create multiple allocations for same allocation id
-		// and eventually throw error.
-		// 2. Blobber will create entry connectionID entry in allocation_connections table for the first op only.
-		if idx == 0 {
-			refs, mask, err := op.Process(mo.allocationObj, mo.connectionID) // Process with each blobber
-			mo.operationMask.And(mask)
-			if err != nil {
-				return err
-			}
-			changes := op.buildChange(refs, uid)
-			mo.changes[idx] = changes
-			continue
-		}
 		wg.Add(1)
 		go func(op Operationer, idx int) {
 			defer wg.Done()
@@ -100,6 +204,12 @@ func (mo *MultiOperation) Process() error {
 	wg.Wait()
 	if ctx.Err() != nil {
 		return <-errs
+	}
+	// Check consensus
+	if mo.operationMask.CountOnes() < mo.consensusThresh {
+		return errors.New("consensus_not_met",
+			fmt.Sprintf("Multioperation failed. Required consensus %d got %d",
+				mo.consensusThresh, mo.operationMask.CountOnes()))
 	}
 
 	// Take transpose of mo.change because it will be easier to iterate mo if it contains blobber changes
