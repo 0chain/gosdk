a54d1fc5c2b750a84613d4ae9661f84aa886748b
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-05-28 18:28:12 +0530

    Fix

--- zboxcore/sdk/allocation.go
+++ zboxcore/sdk/allocation.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math"
 	"mime/multipart"
 	"net/http"
 	"net/url"
--- zcncore/transaction.go
+++ zcncore/transaction.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"net/http"
 	"sync"
 	"time"
