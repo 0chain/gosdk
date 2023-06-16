6ac839fdf5df3da5082e11eaa95949ab6ded1891
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2023-06-17 02:49:23 +0530

    fix append

--- wasmsdk/blobber.go
+++ wasmsdk/blobber.go
@@ -452,7 +452,7 @@ func multiDownload(allocationID, jsonMultiDownloadOptions, authTicket, callbackF
 			mf, _ := fs.(*sys.MemFile)
 			statusResponse.Url = CreateObjectURL(mf.Buffer.Bytes(), "application/octet-stream")
 		}
-		resp[i] = statusResponse
+		resp[ind] = statusResponse
 	}
 
 	respBytes, err := json.Marshal(resp)
