8003d34ae2115603de7c0b7f3f87458a5bfcfa38
Hitenjain14 <hitenjain146@gmail.com>
2023-06-19 01:57:46 +0530

    rmv delete op

--- zboxcore/sdk/chunked_upload.go
+++ zboxcore/sdk/chunked_upload.go
@@ -176,14 +176,6 @@ func CreateChunkedUpload(
 	su.ctx, su.ctxCncl = context.WithCancel(allocationObj.ctx)
 
 	if isUpdate {
-		otr, err := allocationObj.GetRefs(fileMeta.RemotePath, "", "", "", fileref.FILE, "regular", 0, 1)
-		if err != nil {
-			logger.Logger.Error(err)
-			return nil, thrown.New("chunk_upload", err.Error())
-		}
-		if len(otr.Refs) != 1 {
-			return nil, thrown.New("chunk_upload", fmt.Sprintf("Expected refs 1, got %d", len(otr.Refs)))
-		}
 		su.httpMethod = http.MethodPut
 		su.buildChange = func(ref *fileref.FileRef, _ uuid.UUID, ts common.Timestamp) allocationchange.AllocationChange {
 			change := &allocationchange.UpdateFileChange{}
@@ -537,25 +529,25 @@ func (su *ChunkedUpload) Start() error {
 		su.ctx, su.uploadMask, blobbers, su.uploadTimeOut, su.progress.ConnectionID) //nolint: errcheck
 
 	//Check if the allocation is to be repaired or rolled back
-	status, err := su.allocationObj.CheckAllocStatus()
-	if err != nil {
-		logger.Logger.Error("Error checking allocation status", err)
-		if su.statusCallback != nil {
-			su.statusCallback.Error(su.allocationObj.ID, su.fileMeta.RemotePath, su.opCode, err)
-		}
-		return err
-	}
+	// status, err := su.allocationObj.CheckAllocStatus()
+	// if err != nil {
+	// 	logger.Logger.Error("Error checking allocation status", err)
+	// 	if su.statusCallback != nil {
+	// 		su.statusCallback.Error(su.allocationObj.ID, su.fileMeta.RemotePath, su.opCode, err)
+	// 	}
+	// 	return err
+	// }
 
-	if status == Repair {
-		logger.Logger.Info("Repairing allocation")
-		// err = su.allocationObj.RepairAlloc(su.statusCallback)
-		// if err != nil {
-		// 	return err
-		// }
-	}
-	if status != Commit {
-		return ErrRetryOperation
-	}
+	// if status == Repair {
+	// 	logger.Logger.Info("Repairing allocation")
+	// 	// err = su.allocationObj.RepairAlloc(su.statusCallback)
+	// 	// if err != nil {
+	// 	// 	return err
+	// 	// }
+	// }
+	// if status != Commit {
+	// 	return ErrRetryOperation
+	// }
 
 	return su.processCommit()
 }
@@ -733,10 +725,6 @@ func (su *ChunkedUpload) processCommit() error {
 			fmt.Sprintf("Upload commit failed. Required consensus atleast %d, got %d",
 				su.consensus.consensusThresh, consensus))
 
-		if consensus != 0 {
-			logger.Logger.Info("Commit consensus failed, Deleting remote file....")
-			su.allocationObj.deleteFile(su.fileMeta.RemotePath, consensus, consensus) //nolint
-		}
 		if su.statusCallback != nil {
 			su.statusCallback.Error(su.allocationObj.ID, su.fileMeta.RemotePath, su.opCode, err)
 		}
