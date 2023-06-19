ce027bb293d0d71f5cd47465b565803d9a8649b8
Hitenjain14 <hitenjain146@gmail.com>
2023-06-20 01:29:21 +0530

    decrease number of download workers

--- zboxcore/sdk/allocation.go
+++ zboxcore/sdk/allocation.go
@@ -268,7 +268,7 @@ func (a *Allocation) GetBlobberStats() map[string]*BlobberAllocationStats {
 }
 
 const downloadWorkerCount = 10
-const processDownloadWorkerCount = 30
+const processDownloadWorkerCount = 15
 
 func (a *Allocation) InitAllocation() {
 	a.downloadChan = make(chan *DownloadRequest, 100)
