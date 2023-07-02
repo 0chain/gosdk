a534320bbad550ac267564e3c347c3fa689ffe00
Manohar Reddy <b.manu199@gmail.com>
2023-07-02 14:51:53 +0200

    obj.webstreaming

--- wasmsdk/demo/zcn.js
+++ wasmsdk/demo/zcn.js
@@ -161,7 +161,7 @@ async function bulkUpload(options) {
       fileSize: obj.file.size,
       thumbnailBytes:obj.thumbnailBytes?obj.thumbnailBytes.toString():"",
       encrypt:obj.encrypt,
-      webstreaming:true,
+      webstreaming:obj.webstreaming,
       isUpdate:obj.isUpdate,
       isRepair:obj.isRepair,
       numBlocks:obj.numBlocks,
