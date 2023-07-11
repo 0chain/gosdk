d04fc6b97d5c079ac28a0432155d42f4e04cc151
Hitenjain14 <hitenjain146@gmail.com>
2023-07-12 00:30:11 +0530

    rmv streamreader

--- zboxcore/sdk/chunked_upload.go
+++ zboxcore/sdk/chunked_upload.go
@@ -200,13 +200,6 @@ func CreateChunkedUpload(
 	for _, opt := range opts {
 		opt(su)
 	}
-	var streamReader *StreamReader
-	if !su.useFileReader {
-		chunkReadSize := allocationObj.GetChunkReadSize(su.encryptOnUpload)
-		dataChan := make(chan *DataChan, 2)
-		streamReader = NewStreamReader(dataChan)
-		go StartWriteWorker(context.TODO(), fileReader, dataChan, chunkReadSize)
-	}
 
 	if su.progressStorer == nil {
 		su.progressStorer = createFsChunkedUploadProgress(context.Background())
@@ -267,9 +260,6 @@ func CreateChunkedUpload(
 				},
 			},
 		}
-	}
-	if !su.useFileReader {
-		su.fileReader = streamReader
 	}
 	// var cReader ChunkedUploadChunkReader
 	cReader, err := createChunkReader(su.fileReader, fileMeta.ActualSize, int64(su.chunkSize), su.allocationObj.DataShards, su.encryptOnUpload, su.uploadMask, su.fileErasureEncoder, su.fileEncscheme, su.fileHasher)
@@ -347,7 +337,7 @@ type ChunkedUpload struct {
 	maskMu        *sync.Mutex
 	ctx           context.Context
 	ctxCncl       context.CancelFunc
-	useFileReader bool
+	readerCtx     context.Context
 }
 
 // progressID build local progress id with [allocationid]_[Hash(LocalPath+"_"+RemotePath)]_[RemoteName] format
@@ -759,6 +749,5 @@ func getShardSize(dataSize int64, dataShards int, isEncrypted bool) int64 {
 	} else {
 		remainderShards = (r + int64(dataShards) - 1) / int64(dataShards)
 	}
-
 	return n*DefaultChunkSize + remainderShards
 }
