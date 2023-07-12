d293d7d8d0df34f94113dccad0f3e9ea6f97d5ad
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2023-07-12 21:55:58 +0530

    Optimize repair (#1105)
    * rmv streamreader
    
    * optimize repair
    
    * fix lint and test
    
    * rmv readCtx
    
    * fix upload check

--- zboxcore/sdk/repairCallback.go
+++ zboxcore/sdk/repairCallback.go
@@ -21,10 +21,12 @@ func (s *StatusBar) InProgress(allocationId, filePath string, op int, completedB
 func (s *StatusBar) Completed(allocationId, filePath string, filename string, mimetype string, size int, op int) {
 	s.success = true
 	l.Logger.Info("Repair for file completed. File = ", filePath)
+	l.Logger.Info("Repair for file completed. File = ", filePath)
 }
 
 func (s *StatusBar) Error(allocationID string, filePath string, op int, err error) {
 	s.success = false
+	s.err = err
 	defer s.wg.Done()
 	defer mutUnlock(s.allocID)
 
@@ -37,16 +39,19 @@ func (s *StatusBar) Error(allocationID string, filePath string, op int, err erro
 }
 
 func (s *StatusBar) RepairCompleted(filesRepaired int) {
-	defer s.wg.Done()
-	s.success = true
-	mutUnlock(s.allocID)
-	l.Logger.Info("Repair completed. Files repaired = ", filesRepaired)
+	if s.err == nil {
+		s.success = true
+		defer s.wg.Done()
+		mutUnlock(s.allocID)
+		l.Logger.Info("Repair completed. Files repaired = ", filesRepaired)
+	}
 }
 
 type StatusBar struct {
 	wg      *sync.WaitGroup
 	allocID string
 	success bool
+	err     error
 }
 
 func NewRepairBar(allocID string) *StatusBar {
