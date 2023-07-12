7a6bbca368a22d635ec7ca5ae52fb9f89f597a8c
Hitenjain14 <hitenjain146@gmail.com>
2023-07-12 11:15:39 +0530

    fix upload check

--- zboxcore/sdk/chunked_upload.go
+++ zboxcore/sdk/chunked_upload.go
@@ -451,7 +451,7 @@ func (su *ChunkedUpload) process() error {
 
 			if su.fileMeta.ActualSize == 0 {
 				su.fileMeta.ActualSize = su.progress.UploadLength
-			} else if su.fileMeta.ActualSize != su.progress.UploadLength {
+			} else if su.fileMeta.ActualSize != su.progress.UploadLength && su.thumbnailBytes == nil {
 				if su.statusCallback != nil {
 					su.statusCallback.Error(su.allocationObj.ID, su.fileMeta.RemotePath, su.opCode, thrown.New("upload_failed", "Upload failed. Uploaded size does not match with actual size: "+fmt.Sprintf("%d != %d", su.fileMeta.ActualSize, su.progress.UploadLength)))
 				}
