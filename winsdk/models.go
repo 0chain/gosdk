ad956b0d85a3f0f0b459d7377b8ac0f964f4f278
Lz <imlangzi@qq.com>
2023-07-20 10:19:56 +0800

    feat(win): added GetBulkUploadStatus

--- winsdk/models.go
+++ winsdk/models.go
@@ -1,38 +1,8 @@
 package main
 
-import "sync"
-
-type StatusCallback struct {
-	sync.Mutex
-}
-
-func (c *StatusCallback) Started(allocationId, filePath string, op int, totalBytes int) {
-	//c.Callback.Started(allocationId, filePath, op, totalBytes)
-}
-
-func (c *StatusCallback) InProgress(allocationId, filePath string, op int, completedBytes int, data []byte) {
-	//c.Callback.InProgress(allocationId, filePath, op, completedBytes, data)
-}
-
-func (c *StatusCallback) Error(allocationID string, filePath string, op int, err error) {
-	//c.Callback.Error(allocationID, filePath, op, err)
-}
-
-func (c *StatusCallback) Completed(allocationId, filePath string, filename string, mimetype string, size int, op int) {
-	//c.Callback.Completed(allocationId, filePath, filename, mimetype, size, op)
-}
-
-func (c *StatusCallback) CommitMetaCompleted(request, response string, err error) {
-	//c.Callback.CommitMetaCompleted(request, response, err)
-}
-
-func (c *StatusCallback) RepairCompleted(filesRepaired int) {
-	//c.Callback.RepairCompleted(filesRepaired)
-}
-
 type UploadFile struct {
-	Name          string `json:"fileName,omitempty"`
-	Path          string `json:"filePath,omitempty"`
+	Name          string `json:"name,omitempty"`
+	Path          string `json:"path,omitempty"`
 	ThumbnailPath string `json:"thumbnailPath,omitempty"`
 
 	RemotePath string `json:"remotePath,omitempty"`
---
+++ winsdk/status.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"sync"
+
+	lru "github.com/hashicorp/golang-lru/v2"
+)
+
+var (
+	statusCaches, _ = lru.New[string, *StatusCallback](100)
+
+	ErrInvalidUploadID   = errors.New("bulkupload: invalid upload id")
+	ErrInvalidRemotePath = errors.New("bulkupload: invalid remotePath")
+)
+
+type Status struct {
+	Started        bool
+	CompletedBytes int
+	Error          string
+	Completed      bool
+}
+
+type StatusCallback struct {
+	sync.Mutex
+	status map[string]*Status
+}
+
+func (c *StatusCallback) getStatus(remotePath string) *Status {
+	c.Lock()
+	defer c.Unlock()
+
+	s, ok := c.status[remotePath]
+	if !ok {
+		s = &Status{}
+		c.status[remotePath] = s
+	}
+
+	return s
+}
+
+func (c *StatusCallback) Started(allocationId, remotePath string, op int, totalBytes int) {
+	s := c.getStatus(remotePath)
+	s.Started = true
+}
+
+func (c *StatusCallback) InProgress(allocationId, remotePath string, op int, completedBytes int, data []byte) {
+	s := c.getStatus(remotePath)
+	s.CompletedBytes = completedBytes
+}
+
+func (c *StatusCallback) Error(allocationID string, remotePath string, op int, err error) {
+	s := c.getStatus(remotePath)
+	s.Error = err.Error()
+}
+
+func (c *StatusCallback) Completed(allocationId, remotePath string, filename string, mimetype string, size int, op int) {
+	s := c.getStatus(remotePath)
+	s.Completed = true
+}
+
+func (c *StatusCallback) CommitMetaCompleted(request, response string, err error) {
+	//c.Callback.CommitMetaCompleted(request, response, err)
+}
+
+func (c *StatusCallback) RepairCompleted(filesRepaired int) {
+	//c.Callback.RepairCompleted(filesRepaired)
+}
