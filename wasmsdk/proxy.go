d6cb27677d95c94f538c8f50850ebc7a933816de
Manohar Reddy <b.manu199@gmail.com>
2023-08-10 17:04:20 +0200

    gofmt

--- wasmsdk/proxy.go
+++ wasmsdk/proxy.go
@@ -159,6 +159,7 @@ func main() {
 				"isWalletID":             isWalletID,
 				"getLookupHash":          getLookupHash,
 				"createThumbnail":        createThumbnail,
+				"makeSCRestAPICall":      makeSCRestAPICall,
 
 				//blobber
 				"delete":                 Delete,
@@ -204,7 +205,7 @@ func main() {
 				"getAllocationMinLock":       getAllocationMinLock,
 				"getUpdateAllocationMinLock": getUpdateAllocationMinLock,
 				"getAllocationWith":          getAllocationWith,
-				"createfreeallocation": createfreeallocation,
+				"createfreeallocation":       createfreeallocation,
 
 				// readpool
 				"getReadPoolInfo": getReadPoolInfo,
