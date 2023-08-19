04012202695c0f76beeb14cffee68ffac40e84c9
din-mukhammed <dimash.r53@gmail.com>
2023-08-20 02:36:42 +0600

    cleanup

--- zboxcore/sdk/downloadworker.go
+++ zboxcore/sdk/downloadworker.go
@@ -503,29 +503,26 @@ func (req *DownloadRequest) processDownload(ctx context.Context) {
 
 	eg, _ := errgroup.WithContext(ctx)
 	for i := 0; i < n; i++ {
-		j := i
-
 		blocksToDownload := numBlocks
-		if startBlock+int64(j)*numBlocks+numBlocks > endBlock {
-			blocksToDownload = endBlock - (startBlock + int64(j)*numBlocks)
+		if startBlock+int64(i)*numBlocks+numBlocks > endBlock {
+			blocksToDownload = endBlock - (startBlock + int64(i)*numBlocks)
 		}
 		if blocksToDownload <= 0 {
 			continue
 		}
-		func(j int, blocksToDownload int64) {
+		func(i int, blocksToDownload int64) {
 			eg.Go(func() error {
-				data, err := req.getBlocksData(startBlock+int64(j)*numBlocks, blocksToDownload)
+				data, err := req.getBlocksData(startBlock+int64(i)*numBlocks, blocksToDownload)
 				if req.isDownloadCanceled {
 					return errors.New("download_abort", "Download aborted by user")
 				}
 				if err != nil {
-					return errors.Wrap(err, fmt.Sprintf("Download failed for block %d. ", startBlock+int64(j)*numBlocks))
+					return errors.Wrap(err, fmt.Sprintf("Download failed for block %d. ", startBlock+int64(i)*numBlocks))
 				}
-				blocks <- blockData{blockNum: j, data: data}
-
+				blocks <- blockData{blockNum: i, data: data}
 				return nil
 			})
-		}(j, blocksToDownload)
+		}(i, blocksToDownload)
 	}
 	if err := eg.Wait(); err != nil {
 		req.errorCB(err, remotePathCB)
