f9dc5b1d805fdc29f4374a50c3151310844dda30
Yury <yuderbasov@gmail.com>
2023-09-14 22:54:47 +0300

    remove path header, fix ws tag (#1223)
    * remove path header, fix ws tag
    
    * fixed broken build
    
    ---------
    
    Co-authored-by: din-mukhammed <dimash.r53@gmail.com>

--- zboxcore/sdk/blockdownloadworker.go
+++ zboxcore/sdk/blockdownloadworker.go
@@ -124,7 +124,6 @@ func (req *BlockDownloadRequest) downloadBlobberBlock() {
 		}
 
 		header := &DownloadRequestHeader{}
-		header.Path = req.remotefilepath
 		header.PathHash = req.remotefilepathhash
 		header.BlockNum = req.blockNum
 		header.NumBlocks = req.numBlocks
