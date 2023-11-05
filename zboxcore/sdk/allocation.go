cc2c7f1a92edc2991ea48365d5520f06cfdfee2c
Hitenjain14 <hitenjain146@gmail.com>
2023-11-05 13:35:28 +0530

    set numBlock and worker count

--- zboxcore/sdk/allocation.go
+++ zboxcore/sdk/allocation.go
@@ -292,7 +292,7 @@ func (a *Allocation) GetBlobberStats() map[string]*BlobberAllocationStats {
 	return result
 }
 
-const downloadWorkerCount = 10
+const downloadWorkerCount = 2
 
 func (a *Allocation) InitAllocation() {
 	a.downloadChan = make(chan *DownloadRequest, 100)
