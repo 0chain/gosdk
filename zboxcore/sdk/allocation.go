310edbb4d669566acc8cfc6e2940170d2da813fa
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2023-12-14 03:28:08 +0530

    remove batch size (#1327)

--- zboxcore/sdk/allocation.go
+++ zboxcore/sdk/allocation.go
@@ -39,10 +39,9 @@ import (
 )
 
 var (
-	noBLOBBERS       = errors.New("", "No Blobbers set in this allocation")
-	notInitialized   = errors.New("sdk_not_initialized", "Please call InitStorageSDK Init and use GetAllocation to get the allocation object")
-	IsWasm           = false
-	MultiOpBatchSize = 10
+	noBLOBBERS     = errors.New("", "No Blobbers set in this allocation")
+	notInitialized = errors.New("sdk_not_initialized", "Please call InitStorageSDK Init and use GetAllocation to get the allocation object")
+	IsWasm         = false
 )
 
 const (
@@ -242,7 +241,6 @@ func GetWritePriceRange() (PriceRange, error) {
 func SetWasm() {
 	IsWasm = true
 	BatchSize = 5
-	MultiOpBatchSize = 7
 }
 
 func getPriceRange(name string) (PriceRange, error) {
@@ -841,11 +839,6 @@ func (a *Allocation) DoMultiOperation(operations []OperationRequest) error {
 		}
 
 		for ; i < len(operations); i++ {
-			if len(mo.operations) >= MultiOpBatchSize {
-				// max batch size reached, commit
-				connectionID = zboxutil.NewConnectionId()
-				break
-			}
 			op := operations[i]
 			remotePath := op.RemotePath
 			parentPaths := GenerateParentPaths(remotePath)
