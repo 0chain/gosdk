11fffde7a4221869dcb2bf2c177f70bd256ae71f
peterlimg <peterlimg@protonmail.com>
2023-12-16 15:36:00 +1100

    Add `force` param for getAllocationBlobbers wasm

--- zboxcore/sdk/sdk.go
+++ zboxcore/sdk/sdk.go
@@ -1031,6 +1031,7 @@ func GetAllocationBlobbers(
 	datashards, parityshards int,
 	size int64,
 	readPrice, writePrice PriceRange,
+	force ...bool,
 ) ([]string, error) {
 	var allocationRequest = map[string]interface{}{
 		"data_shards":       datashards,
@@ -1044,6 +1045,9 @@ func GetAllocationBlobbers(
 
 	params := make(map[string]string)
 	params["allocation_data"] = string(allocationData)
+	if len(force) > 0 && force[0] {
+		params["force"] = strconv.FormatBool(force[0])
+	}
 
 	allocBlobber, err := zboxutil.MakeSCRestAPICall(STORAGE_SCADDRESS, "/alloc_blobbers", params, nil)
 	if err != nil {
