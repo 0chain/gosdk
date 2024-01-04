4ed3f423387ed202fef7a9b61f2f1dee200d3bd7
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2024-01-05 00:59:16 +0530

    repair in batches (#1347)
    * repair in batches
    
    * fix lint
    
    * fix unit test
    
    * fix batch size
    
    ---------
    
    Co-authored-by: Yury <yuderbasov@gmail.com>

--- zboxcore/sdk/commitworker.go
+++ zboxcore/sdk/commitworker.go
@@ -98,6 +98,11 @@ func (commitreq *CommitRequest) processCommit() {
 	for _, change := range commitreq.changes {
 		paths = append(paths, change.GetAffectedPath()...)
 	}
+	if len(paths) == 0 {
+		l.Logger.Info("Nothing to commit")
+		commitreq.result = SuccessCommitResult()
+		return
+	}
 	var req *http.Request
 	var lR ReferencePathResult
 	req, err := zboxutil.NewReferencePathRequest(commitreq.blobber.Baseurl, commitreq.allocationID, commitreq.allocationTx, paths)
@@ -105,12 +110,6 @@ func (commitreq *CommitRequest) processCommit() {
 		l.Logger.Error("Creating ref path req", err)
 		return
 	}
-	if len(paths) == 0 {
-		l.Logger.Info("Nothing to commit")
-		commitreq.result = SuccessCommitResult()
-		return
-	}
-
 	ctx, cncl := context.WithTimeout(context.Background(), (time.Second * 30))
 	err = zboxutil.HttpDo(ctx, cncl, req, func(resp *http.Response, err error) error {
 		if err != nil {
