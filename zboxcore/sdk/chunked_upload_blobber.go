e1c1ae56452aec63ae24a4f1818ae4ae08286224
Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>
2024-03-01 00:24:23 +0530

    fix upload timeout (#1412)

--- zboxcore/sdk/chunked_upload_blobber.go
+++ zboxcore/sdk/chunked_upload_blobber.go
@@ -76,9 +76,9 @@ func (sb *ChunkedUploadBlobber) sendUploadRequest(
 			var (
 				shouldContinue bool
 			)
-
+			var req *fasthttp.Request
 			for i := 0; i < 3; i++ {
-				req, err := zboxutil.NewFastUploadRequest(
+				req, err = zboxutil.NewFastUploadRequest(
 					sb.blobber.Baseurl, su.allocationObj.ID, su.allocationObj.Tx, dataBuffers[ind].Bytes(), su.httpMethod)
 				if err != nil {
 					return err
@@ -129,12 +129,14 @@ func (sb *ChunkedUploadBlobber) sendUploadRequest(
 					return
 				}()
 
-				if err == nil {
-					return nil
-				}
 				if shouldContinue {
 					continue
 				}
+
+				if err != nil {
+					return err
+				}
+
 				break
 			}
 			return err
