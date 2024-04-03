d937d845d7d37f1e29d4eb5252968048f408da43
storybehind <arunmail12@gmail.com>
2024-04-03 21:19:02 +0530

    get client state fields in getWalletBalance wasm api

--- wasmsdk/zcn.go
+++ wasmsdk/zcn.go
@@ -4,6 +4,9 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+
 	"github.com/0chain/gosdk/zboxcore/sdk"
 	"github.com/0chain/gosdk/zcncore"
 )
@@ -11,6 +14,9 @@ import (
 type Balance struct {
 	ZCN float64 `json:"zcn"`
 	USD float64 `json:"usd"`
+	TxnHash string  `json:"txn"`
+	Round   int64   `json:"round"`
+	Nonce   int64   `json:"nonce"`
 }
 
 func getWalletBalance(clientId string) (*Balance, error) {
@@ -30,9 +36,25 @@ func getWalletBalance(clientId string) (*Balance, error) {
 		return nil, err
 	}
 
+	clientStateStr, err := zcncore.GetClientState(clientId)
+	if err != nil {
+		return nil, err
+	}
+	var clientState struct {
+		TxnHash string  `json:"txn"`
+		Round   int64   `json:"round"`
+		Nonce   int64   `json:"nonce"`
+	}
+	err = json.Unmarshal([]byte(clientStateStr), &clientState)
+	if err != nil {
+		return nil, errors.New(`error unmarshaling client state: ` + err.Error())
+	}
 	return &Balance{
 		ZCN: zcnToken,
 		USD: usd,
+		TxnHash: clientState.TxnHash,
+		Round: clientState.Round,
+		Nonce: clientState.Nonce,
 	}, nil
 }
 
