50239a804853465c49f471b66ce6be3179c572bf
Hitenjain14 <hitenjain146@gmail.com>
2024-05-01 20:04:43 +0530

    update file reader and writer for wasm

--- wasmsdk/jsbridge/file_writer.go
+++ wasmsdk/jsbridge/file_writer.go
@@ -5,53 +5,74 @@ package jsbridge
 
 import (
 	"errors"
+	"io"
 	"io/fs"
-	"sync"
 	"syscall/js"
 )
-
-var jsFileWriterMutex sync.Mutex
 
 type FileWriter struct {
 	writableStream js.Value
 	uint8Array     js.Value
 	fileHandle     js.Value
 	bufLen         int
+	buf            []byte
+	bufWriteOffset int
 }
 
+const writeBufferSize = 4 * 1024 * 1024 //4MB
+
+// len(p) will always be <= 64KB
 func (w *FileWriter) Write(p []byte) (int, error) {
-	//js.Value doesn't work in parallel invoke
-	jsFileWriterMutex.Lock()
-	defer jsFileWriterMutex.Unlock()
-
-	if w.bufLen != len(p) {
-		w.bufLen = len(p)
-		w.uint8Array = js.Global().Get("Uint8Array").New(w.bufLen)
+	//copy bytes to buf
+	if w.bufWriteOffset+len(p) > len(w.buf) {
+		return 0, io.ErrShortWrite
 	}
-	js.CopyBytesToJS(w.uint8Array, p)
-	_, err := Await(w.writableStream.Call("write", w.uint8Array))
-	if len(err) > 0 && !err[0].IsNull() {
-		return 0, errors.New("file_writer: " + err[0].String())
-	}
-	return len(p), nil
-}
-
-func (w *FileWriter) WriteAt(p []byte, offset int64) (int, error) {
-	uint8Array := js.Global().Get("Uint8Array").New(len(p))
-	js.CopyBytesToJS(uint8Array, p)
-	options := js.Global().Get("Object").New()
-	options.Set("type", "write")
-	options.Set("position", offset)
-	options.Set("data", uint8Array)
-	options.Set("size", len(p))
-	_, err := Await(w.writableStream.Call("write", options))
-	if len(err) > 0 && !err[0].IsNull() {
-		return 0, errors.New("file_writer: " + err[0].String())
+	n := copy(w.buf[w.bufWriteOffset:], p)
+	w.bufWriteOffset += n
+	if w.bufWriteOffset == len(w.buf) {
+		//write to file
+		if w.bufLen != len(w.buf) {
+			w.bufLen = len(w.buf)
+			w.uint8Array = js.Global().Get("Uint8Array").New(w.bufLen)
+		}
+		js.CopyBytesToJS(w.uint8Array, w.buf)
+		_, err := Await(w.writableStream.Call("write", w.uint8Array))
+		if len(err) > 0 && !err[0].IsNull() {
+			return 0, errors.New("file_writer: " + err[0].String())
+		}
+		//reset buffer
+		w.bufWriteOffset = 0
 	}
 	return len(p), nil
 }
+
+// func (w *FileWriter) WriteAt(p []byte, offset int64) (int, error) {
+// 	uint8Array := js.Global().Get("Uint8Array").New(len(p))
+// 	js.CopyBytesToJS(uint8Array, p)
+// 	options := js.Global().Get("Object").New()
+// 	options.Set("type", "write")
+// 	options.Set("position", offset)
+// 	options.Set("data", uint8Array)
+// 	options.Set("size", len(p))
+// 	_, err := Await(w.writableStream.Call("write", options))
+// 	if len(err) > 0 && !err[0].IsNull() {
+// 		return 0, errors.New("file_writer: " + err[0].String())
+// 	}
+// 	return len(p), nil
+// }
 
 func (w *FileWriter) Close() error {
+
+	if w.bufWriteOffset > 0 {
+		w.buf = w.buf[:w.bufWriteOffset]
+		uint8Array := js.Global().Get("Uint8Array").New(len(w.buf))
+		js.CopyBytesToJS(uint8Array, w.buf)
+		_, err := Await(w.writableStream.Call("write", uint8Array))
+		if len(err) > 0 && !err[0].IsNull() {
+			return errors.New("file_writer: " + err[0].String())
+		}
+	}
+
 	_, err := Await(w.writableStream.Call("close"))
 	if len(err) > 0 && !err[0].IsNull() {
 		return errors.New("file_writer: " + err[0].String())
@@ -99,5 +120,6 @@ func NewFileWriter(filename string) (*FileWriter, error) {
 	return &FileWriter{
 		writableStream: writableStream[0],
 		fileHandle:     fileHandle[0],
+		buf:            make([]byte, writeBufferSize),
 	}, nil
 }
