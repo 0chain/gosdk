68ce31d333de92ca67b9cb5fc0c73ac82249c7aa
storybehind <arunmail12@gmail.com>
2024-05-18 01:40:40 +0530

    include pkg dir

---
+++ core/imageutil/image_rs/pkg/image_rs_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function thumbnail(a: number, b: number, c: number, d: number): number;
+export function allocate(a: number): number;
+export function deallocate(a: number, b: number): void;
+export function thumbnail_js(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
