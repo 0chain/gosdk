68e6d64812a4c5be81d8a61bb37a6fe78d0075af
Hitenjain14 <hitenjain146@gmail.com>
2024-05-18 21:37:48 +0530

    revert err msg change

--- zboxcore/sdk/multi_operation_worker.go
+++ zboxcore/sdk/multi_operation_worker.go
@@ -347,8 +347,7 @@ func (mo *MultiOperation) Process() error {
 		for _, op := range mo.operations {
 			op.Error(mo.allocationObj, mo.getConsensus(), err)
 		}
-		l.Logger.Error("Consensus not met. Required ", mo.consensusThresh, " got ", mo.getConsensus(), " error: ", err)
-		return errors.New("consensus_not_met", fmt.Sprintf("Consensus not met. Required %d got %d", mo.consensusThresh, mo.getConsensus()))
+		return err
 	} else {
 		for _, op := range mo.operations {
 			op.Completed(mo.allocationObj)
