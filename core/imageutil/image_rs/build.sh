d6ae6c44bbbe4d1ccf6d1c008e48f05c0c9a93ec
storybehind <arunmail12@gmail.com>
2024-05-21 12:38:26 +0530

    provide full path to wasm-pack

--- core/imageutil/image_rs/build.sh
+++ core/imageutil/image_rs/build.sh
@@ -3,6 +3,6 @@
 $HOME/.cargo/bin/cargo wasi build --release
 cp target/wasm32-wasi/release/image_rs.wasm .
 
-wasm-pack build --target web
+$HOME/.cargo/bin/wasm-pack build --target web
 
 echo "Done!"
