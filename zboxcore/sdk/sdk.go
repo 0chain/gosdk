e12145ef37ad1e1f8807ad4363c1372bbabb28cd
pewssh <pewssh@gmail.com>
2024-07-16 23:19:36 +0545

    updated limit params

--- zboxcore/sdk/sdk.go
+++ zboxcore/sdk/sdk.go
@@ -1106,6 +1106,8 @@ func GetAllocationBlobbers(
 
 	params := make(map[string]string)
 	params["allocation_data"] = string(allocationData)
+	params["limit"] = string(rune(datashards + parityshards))
+
 	if len(force) > 0 && force[0] {
 		params["force"] = strconv.FormatBool(force[0])
 	}
