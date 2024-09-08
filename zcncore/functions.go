ef8a452a46110af8542b8549f62318cf1e9486d3
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-09-08 23:38:38 +0530

    Debug

--- zcncore/functions.go
+++ zcncore/functions.go
@@ -2,7 +2,7 @@ package zcncore
 
 import "github.com/0chain/gosdk/core/transaction"
 
-func StorageScUpdateConfig(input string) (err error) {
+func StorageScUpdateConfig(input interface{}) (err error) {
 	_, _, _, _, err = transaction.SmartContractTxn(StorageSmartContractAddress, transaction.SmartContractTxnData{
 		Name:      transaction.STORAGESC_UPDATE_SETTINGS,
 		InputArgs: input,
