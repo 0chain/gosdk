5e19f7a24a72ca5617761a80c3d730cb18228338
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-09-18 03:48:34 +0530

    Fix sc rest api call

--- core/client/http.go
+++ core/client/http.go
@@ -6,16 +6,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/0chain/errors"
+	"github.com/0chain/gosdk/core/conf"
 	"github.com/0chain/gosdk/core/util"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/shopspring/decimal"
 	"io"
 	"io/ioutil"
+	"log"
 	"math"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -75,18 +78,14 @@ func NewRetryableClient(retryMax int) *retryablehttp.Client {
 	return client
 }
 
-// MakeSCRestAPICall calls smart contract with provided address
-// and makes retryable request to smart contract resource with provided relative path using params.
-func MakeSCRestAPICall(scAddress, relativePath string, params map[string]string, restApiUrls ...string) ([]byte, error) {
-	var (
-		resMaxCounterBody []byte
-
-		hashMaxCounter int
-		hashCounters   = make(map[string]int)
-
-		sharders = extractSharders()
-
-		lastErrMsg string
+// MakeSCRestAPICall makes a rest api call to the sharders.
+//   - scAddress is the address of the smart contract
+//   - relativePath is the relative path of the api
+//   - params is the query parameters
+//   - handler is the handler function to handle the response
+func MakeSCRestAPICall(scAddress string, relativePath string, params map[string]string, restApiUrls ...string) ([]byte, error) {
+	const (
+		consensusThresh = float32(25.0)
 	)
 
 	restApiUrl := ScRestApiUrl
@@ -94,40 +93,105 @@ func MakeSCRestAPICall(scAddress, relativePath string, params map[string]string,
 		restApiUrl = restApiUrls[0]
 	}
 
+	sharders := nodeClient.Network().Sharders
+	responses := make(map[int]int)
+	mu := &sync.Mutex{}
+	entityResult := make(map[string][]byte)
+	var retObj []byte
+	maxCount := 0
+	dominant := 200
+	wg := sync.WaitGroup{}
+
+	cfg, err := conf.GetClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
 	for _, sharder := range sharders {
-		var (
-			retryableClient = NewRetryableClient(5)
-			u               = makeScURL(params, sharder, restApiUrl, scAddress, relativePath)
-		)
+		wg.Add(1)
+		go func(sharder string) {
+			defer wg.Done()
+			urlString := fmt.Sprintf("%v/%v%v%v", sharder, restApiUrl, scAddress, relativePath)
+			urlObj, err := url.Parse(urlString)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			q := urlObj.Query()
+			for k, v := range params {
+				q.Add(k, v)
+			}
+			urlObj.RawQuery = q.Encode()
+			client := &http.Client{Transport: DefaultTransport}
+			response, err := client.Get(urlObj.String())
+			if err != nil {
+				nodeClient.sharders.Fail(sharder)
+				return
+			}
 
-		resp, err := retryableClient.Get(u.String())
-		if err != nil {
-			lastErrMsg = fmt.Sprintf("error while requesting sharders: %v", err)
-			continue
-		}
-		hash, resBody, err := hashAndBytesOfReader(resp.Body)
-		_ = resp.Body.Close()
-		if err != nil {
-			lastErrMsg = fmt.Sprintf("error while reading response body: %v", err)
-			continue
-		}
-		if resp.StatusCode != http.StatusOK {
-			lastErrMsg = fmt.Sprintf("response status is not OK; response body: %s", string(resBody))
-			continue
-		}
+			defer response.Body.Close()
+			entityBytes, _ := ioutil.ReadAll(response.Body)
+			mu.Lock()
+			if response.StatusCode > http.StatusBadRequest {
+				nodeClient.sharders.Fail(sharder)
+			} else {
+				nodeClient.sharders.Success(sharder)
+			}
+			responses[response.StatusCode]++
+			if responses[response.StatusCode] > maxCount {
+				maxCount = responses[response.StatusCode]
+			}
 
-		hashCounters[hash]++
-		if hashCounters[hash] > hashMaxCounter {
-			hashMaxCounter = hashCounters[hash]
-			resMaxCounterBody = resBody
-		}
+			if isCurrentDominantStatus(response.StatusCode, responses, maxCount) {
+				dominant = response.StatusCode
+				retObj = entityBytes
+			}
+
+			entityResult[sharder] = entityBytes
+			nodeClient.sharders.Success(sharder)
+			mu.Unlock()
+		}(sharder)
+	}
+	wg.Wait()
+
+	rate := float32(maxCount*100) / float32(cfg.SharderConsensous)
+	if rate < consensusThresh {
+		err = errors.New("consensus_failed", "consensus failed on sharders")
 	}
 
-	if hashMaxCounter == 0 {
-		return nil, errors.New("request_sharders", "no valid responses, last err: "+lastErrMsg)
+	if dominant != 200 {
+		var objmap map[string]json.RawMessage
+		err := json.Unmarshal(retObj, &objmap)
+		if err != nil {
+			return nil, errors.New("", string(retObj))
+		}
+
+		var parsed string
+		err = json.Unmarshal(objmap["error"], &parsed)
+		if err != nil || parsed == "" {
+			return nil, errors.New("", string(retObj))
+		}
+
+		return nil, errors.New("", parsed)
 	}
 
-	return resMaxCounterBody, nil
+	if rate > consensusThresh {
+		return retObj, nil
+	}
+	return nil, err
+}
+
+// isCurrentDominantStatus determines whether the current response status is the dominant status among responses.
+//
+// The dominant status is where the response status is counted the most.
+// On tie-breakers, 200 will be selected if included.
+//
+// Function assumes runningTotalPerStatus can be accessed safely concurrently.
+func isCurrentDominantStatus(respStatus int, currentTotalPerStatus map[int]int, currentMax int) bool {
+	// mark status as dominant if
+	// - running total for status is the max and response is 200 or
+	// - running total for status is the max and count for 200 is lower
+	return currentTotalPerStatus[respStatus] == currentMax && (respStatus == 200 || currentTotalPerStatus[200] < currentMax)
 }
 
 // hashAndBytesOfReader computes hash of readers data and returns hash encoded to hex and bytes of reader data.
