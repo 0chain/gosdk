4d031a3d90f5ad4ba043e5845d46b8de4617db7c
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2024-09-24 21:15:24 +0200

    fix: debug

--- zcncore/transactionauth_mobile.go
+++ zcncore/transactionauth_mobile.go
@@ -19,8 +19,8 @@ func newTransactionWithAuth(cb TransactionCallback, txnFee string, nonce int64) 
 }
 
 // ExecuteSmartContract prepare and send a smart contract transaction to the blockchain
-func (t *TransactionWithAuth) ExecuteSmartContracts(address string, methodName string, val string, input string) error {
-	return nil
+func (t *TransactionWithAuth) ExecuteSmartContracts(address string, methodName string, val string, input string) (*transaction.Transaction, error) {
+	return nil, nil
 }
 
 func (ta *TransactionWithAuth) ExecuteSmartContract(address, methodName string,
