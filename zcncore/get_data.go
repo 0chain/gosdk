b661069f9157ca17b1bbdf62995e1b8304bc53ba
peterlimg <peterlimg@protonmail.com>
2024-11-26 11:33:33 +1100

    Log set wallet

--- zcncore/get_data.go
+++ zcncore/get_data.go
@@ -86,6 +86,8 @@ func SetWalletInfo(jsonWallet, sigScheme string, splitKeyWallet bool) error {
 		return errors.New("invalid jsonWallet: " + err.Error())
 	}
 
+	fmt.Println("set wallet info", wallet)
+
 	client.SetWallet(wallet)
 	client.SetSignatureScheme(sigScheme)
 
