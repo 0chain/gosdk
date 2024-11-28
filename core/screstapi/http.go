408758b70a45793b753b3322dddc36db230764a4
smaulik13 <mauliksinghal4@gmail.com>
2024-11-28 21:18:13 +0530

    fix

--- core/screstapi/http.go
+++ core/screstapi/http.go
@@ -28,7 +28,7 @@ var urlPathSharderToZboxMap = map[string]string{
 
 func MakeSCRestAPICall(scAddress string, relativePath string, params map[string]string, restApiUrls ...string) (resp []byte, err error) {
 	_, ok := urlPathSharderToZboxMap[relativePath]
-	if client.IsWebAppFlow && ok {
+	if client.IsAppFlow && ok {
 		resp, err = MakeSCRestAPICallToZbox(urlPathSharderToZboxMap[relativePath], params)
 		if err != nil {
 			resp, err = client.MakeSCRestAPICallToSharder(scAddress, relativePath, params)
