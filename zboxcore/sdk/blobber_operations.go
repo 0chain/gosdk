afddeb587d56ad201eb6d0ae81577a9e00af802e
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-12-22 23:53:18 +0530

    Fix sign

--- zboxcore/sdk/blobber_operations.go
+++ zboxcore/sdk/blobber_operations.go
@@ -192,7 +192,7 @@ func UpdateAllocation(
 func GetUpdateAllocTicket(allocationID, userID, operationType string, roundExpiry int64) (string, error) {
 	payload := fmt.Sprintf("%s:%d:%s:%s", allocationID, roundExpiry, userID, operationType)
 
-	signature, err := client.Sign(encryption.Hash(payload))
+	signature, err := client.Sign(hex.EncodeToString([]byte(payload)))
 	if err != nil {
 		return "", err
 	}
