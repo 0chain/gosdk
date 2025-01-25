220101ee6940094158440822a2999eafa35c500a
harsh4723 <harsh4723@rediffmail.com>
2025-01-25 23:20:27 +0530

    test

--- zboxcore/sdk/rollback.go
+++ zboxcore/sdk/rollback.go
@@ -68,6 +68,7 @@ func GetWritemarker(allocID, allocTx, sig, id, baseUrl string, clientId ...strin
 
 	req, err := zboxutil.NewWritemarkerRequest(baseUrl, allocID, allocTx, sig, clientId...)
 	fmt.Printf("Harsh getwritemarker req baseurl%s allocationTx %s", baseUrl, allocTx)
+	fmt.Printf("Harsh getwritemarker reqcleintid %s", clientId)
 	if err != nil {
 		return nil, err
 	}
