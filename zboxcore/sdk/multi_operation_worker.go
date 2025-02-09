0bda1976e5fcc91e385f459bf35437f08d96b48e
smaulik13 <mauliksinghal4@gmail.com>
2025-02-09 17:49:57 +0530

    logging 2

--- zboxcore/sdk/multi_operation_worker.go
+++ zboxcore/sdk/multi_operation_worker.go
@@ -322,11 +322,15 @@ func (mo *MultiOperation) Process() error {
 		l.Logger.Error("consensus not met", activeBlobbers, mo.consensusThresh)
 		return errors.New("consensus_not_met", fmt.Sprintf("Active blobbers %d is less than consensus threshold %d", activeBlobbers, mo.consensusThresh))
 	}
+
+	allocJson, _ := json.MarshalIndent(mo, "", "")
+	logger.Logger.Debug("mo allocation json", string(allocJson))
+	l.Logger.Debug("mo allocation json 1", string(allocJson))
+
 	if mo.allocationObj.StorageVersion == StorageV2 {
-		allocJson, _ := json.MarshalIndent(mo, "", "")
-		l.Logger.Info("mo allocation json", string(allocJson))
 		return mo.commitV2()
 	}
+
 	commitReqs := make([]*CommitRequest, activeBlobbers)
 	start = time.Now()
 	wg.Add(activeBlobbers)
