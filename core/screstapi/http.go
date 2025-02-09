062c608fbe568423d6b0886657d94b533f40d33c
smaulik13 <mauliksinghal4@gmail.com>
2025-02-09 14:20:41 +0530

    test 5

--- core/screstapi/http.go
+++ core/screstapi/http.go
@@ -23,7 +23,7 @@ var urlPathSharderToZboxMap = map[string]string{
 	"client/get/balance":             "/balance",
 	"/v1/not_processed_burn_tickets": "/not_processed_burn_tickets",
 	"/allocations":                   "/getAllocations",
-	"/allocation":                    "/getAllocation",
+	// "/allocation":                    "/getAllocation",
 }
 
 func MakeSCRestAPICall(scAddress string, relativePath string, params map[string]string, restApiUrls ...string) (resp []byte, err error) {
