07ad13e6e58bef69137a602520c40187fdad65c9
smaulik13 <mauliksinghal4@gmail.com>
2025-02-10 14:46:34 +0530

    revert changes

--- zboxcore/sdk/sdk.go
+++ zboxcore/sdk/sdk.go
@@ -633,29 +633,6 @@ func GetAllocation(allocationID string) (*Allocation, error) {
 		return nil, errors.New("allocation_decode_error", "Error decoding the allocation: "+err.Error()+" "+string(allocationBytes))
 	}
 
-	allocationBytesSh, err := client.MakeSCRestAPICallToSharder(STORAGE_SCADDRESS, "/allocation", params)
-	if err != nil {
-		return nil, errors.New("allocation_fetch_error", "Error fetching the allocation."+err.Error())
-	}
-
-	allocationObjSharder := &Allocation{}
-	err = json.Unmarshal(allocationBytesSh, allocationObjSharder)
-	if err != nil {
-		return nil, errors.New("allocation_decode_error", "Error decoding the allocation: "+err.Error()+" "+string(allocationBytes))
-	}
-
-	alloczbox, err := json.MarshalIndent(allocationObj, "", "")
-	if err != nil {
-		return nil, errors.New("allocation_decode_error", "Error marshaling the allocation: "+err.Error())
-	}
-	fmt.Println("allocation_decode for 0box", string(alloczbox))
-
-	allocSharder, err := json.MarshalIndent(allocationObjSharder, "", "")
-	if err != nil {
-		return nil, errors.New("allocation_decode_error", "Error marshaling the allocation: "+err.Error())
-	}
-	fmt.Println("allocation_decode for sharder", string(allocSharder))
-
 	allocationObj.numBlockDownloads = numBlockDownloads
 	allocationObj.InitAllocation()
 	return allocationObj, nil
