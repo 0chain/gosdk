8ed53bb3ce6a12948cfc83a83ebfdf0c081e8a8c
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2025-04-03 23:49:44 +0530

    Fix sync

--- zboxcore/sdk/sync.go
+++ zboxcore/sdk/sync.go
@@ -200,12 +200,12 @@ func isParentFolderExists(lFDiff []FileDiff, path string) bool {
 	return false
 }
 
-func findDelta(rMap map[string]FileInfo, lMap map[string]FileInfo, prevMap map[string]FileInfo, localRootPath string) []FileDiff {
-	var lFDiff []FileDiff
+func findDelta(remoteMap, localMap, prevMap map[string]FileInfo, localRootPath string) []FileDiff {
+	var fileDiffs []FileDiff
+	rMod, lMod, rDelMap := make(map[string]FileInfo), make(map[string]FileInfo), make(map[string]bool)
 
 	// Create a remote hash map and find modifications
-	rMod := make(map[string]FileInfo)
-	for rFile, rInfo := range rMap {
+	for rFile, rInfo := range remoteMap {
 		if pm, ok := prevMap[rFile]; ok {
 			// Remote file existed in previous sync also
 			if pm.Hash != rInfo.Hash {
@@ -215,94 +215,63 @@ func findDelta(rMap map[string]FileInfo, lMap map[string]FileInfo, prevMap map[s
 		}
 	}
 
-	// Create a local hash map and find modification
-	lMod := make(map[string]FileInfo)
-	for lFile, lInfo := range lMap {
-		if pm, ok := rMap[lFile]; ok {
-			// Local file existed in previous sync also
-			if pm.Hash != lInfo.Hash {
-				// File modified in local
-				lMod[lFile] = lInfo
-			}
+	// Identify modified local files
+	for lFile, lInfo := range localMap {
+		if remote, exists := remoteMap[lFile]; exists && (remote.Hash != lInfo.Hash) {
+			lMod[lFile] = lInfo
 		}
 	}
 
-	// Iterate remote list and get diff
-	rDelMap := make(map[string]string)
-	for rPath := range rMap {
-		op := Download
-		bRemoteModified := false
-		bLocalModified := false
-		if _, ok := rMod[rPath]; ok {
-			bRemoteModified = true
-		}
-		if _, ok := lMod[rPath]; ok {
-			bLocalModified = true
-			delete(lMap, rPath)
-		}
-		if bRemoteModified && bLocalModified {
-			op = Conflict
-		} else if bLocalModified {
-			op = Update
-		} else if _, ok := lMap[rPath]; ok {
-			// No conflicts and file exists locally
-			delete(lMap, rPath)
+	// Determine sync actions for remote files
+	for rPath := range remoteMap {
+		var op = Download
+		if _, remoteModified := rMod[rPath]; remoteModified {
+			if _, localModified := lMod[rPath]; localModified {
+				op = Conflict
+			} else {
+				op = Update
+			}
+		} else if _, exists := localMap[rPath]; exists {
+			delete(localMap, rPath)
 			continue
-		} else if _, ok := prevMap[rPath]; ok {
+		} else if _, existedBefore := prevMap[rPath]; existedBefore {
 			op = Delete
-			// Remote allows delete directory skip individual file deletion
-			rDelMap[rPath] = "d"
-			rDir, _ := filepath.Split(rPath)
-			rDir = strings.TrimRight(rDir, "/")
-			if _, ok := rDelMap[rDir]; ok {
-				continue
-			}
+			rDelMap[rPath] = true
 		}
-		lFDiff = append(lFDiff, FileDiff{Path: rPath, Op: op, Type: rMap[rPath].Type})
+		fileDiffs = append(fileDiffs, FileDiff{Path: rPath, Op: op, Type: remoteMap[rPath].Type})
 	}
 
-	// Upload all local files
-	for lPath := range lMap {
-		op := Upload
-		if _, ok := lMod[lPath]; ok {
+	// Determine sync actions for local files
+	for lPath := range localMap {
+		var op = Upload
+		if _, modified := lMod[lPath]; modified {
 			op = Update
-		} else if _, ok := prevMap[lPath]; ok {
+		} else if _, existedBefore := prevMap[lPath]; existedBefore {
 			op = LocalDelete
 		}
+
+		// Ensure directories are not added for upload
 		if op != LocalDelete {
-			// Skip if it is a directory
 			lAbsPath := filepath.Join(localRootPath, lPath)
-			fInfo, err := sys.Files.Stat(lAbsPath)
-			if err != nil {
-				continue
-			}
-			if fInfo.IsDir() {
+			if fInfo, err := os.Stat(lAbsPath); err == nil && fInfo.IsDir() {
 				continue
 			}
 		}
-		lFDiff = append(lFDiff, FileDiff{Path: lPath, Op: op, Type: lMap[lPath].Type})
+		fileDiffs = append(fileDiffs, FileDiff{Path: lPath, Op: op, Type: localMap[lPath].Type})
 	}
 
-	// If there are differences, remove childs if the parent folder is deleted
-	if len(lFDiff) > 0 {
-		sort.SliceStable(lFDiff, func(i, j int) bool { return lFDiff[i].Path < lFDiff[j].Path })
-		l.Logger.Debug("Sorted diff: ", lFDiff)
-		var newlFDiff []FileDiff
-		for _, f := range lFDiff {
-			if f.Op == LocalDelete || f.Op == Delete {
-				if !isParentFolderExists(newlFDiff, f.Path) {
-					newlFDiff = append(newlFDiff, f)
-				}
-			} else {
-				// Add only files for other Op
-				if f.Type == fileref.FILE {
-					newlFDiff = append(newlFDiff, f)
-				}
-			}
+	// Remove child files if parent directory is deleted
+	sort.SliceStable(fileDiffs, func(i, j int) bool { return fileDiffs[i].Path < fileDiffs[j].Path })
+	var cleanedDiffs []FileDiff
+	for _, f := range fileDiffs {
+		if (f.Op == Delete || f.Op == LocalDelete) && isParentFolderExists(cleanedDiffs, f.Path) {
+			continue
 		}
-		return newlFDiff
+		if f.Type == fileref.FILE || f.Op == Delete || f.Op == LocalDelete {
+			cleanedDiffs = append(cleanedDiffs, f)
+		}
 	}
-	return lFDiff
+	return cleanedDiffs
 }
 
 // GetAllocationDiff retrieves the difference between the remote and local filesystem representation of the allocation
@@ -311,47 +280,36 @@ func findDelta(rMap map[string]FileInfo, lMap map[string]FileInfo, prevMap map[s
 //   - localFileFilters is the list of local file filters
 //   - remoteExcludePath is the list of remote exclude paths
 //   - remotePath is the remote path of the allocation
-func (a *Allocation) GetAllocationDiff(lastSyncCachePath string, localRootPath string, localFileFilters []string, remoteExcludePath []string, remotePath string) ([]FileDiff, error) {
+func (a *Allocation) GetAllocationDiff(lastSyncCachePath, localRootPath string, localFileFilters, remoteExcludePath []string, remotePath string) ([]FileDiff, error) {
 	var lFdiff []FileDiff
 	prevRemoteFileMap := make(map[string]FileInfo)
-	// 1. Validate localSycnCachePath
+
+	// 1. Validate and Load Previous Sync Cache
 	if len(lastSyncCachePath) > 0 {
-		// Validate cache path
-		fileInfo, err := sys.Files.Stat(lastSyncCachePath)
-		if err == nil {
-			if fileInfo.IsDir() {
-				return lFdiff, errors.Wrap(err, "invalid file cache.")
-			}
-			content, err := os.ReadFile(lastSyncCachePath)
-			if err != nil {
-				return lFdiff, errors.New("", "can't read cache file.")
-			}
-			err = json.Unmarshal(content, &prevRemoteFileMap)
-			if err != nil {
-				return lFdiff, errors.New("", "invalid cache content.")
+		if content, err := os.ReadFile(lastSyncCachePath); err == nil {
+			if err := json.Unmarshal(content, &prevRemoteFileMap); err != nil {
+				return lFdiff, errors.Wrap(err, "Invalid cache content.")
 			}
 		}
 	}
 
-	// 2. Build a map for exclude path
+	// 2. Build Exclusion Map
 	exclMap := getRemoteExcludeMap(remoteExcludePath)
 
-	// 3. Get flat file list from remote
+	// 3. Get Remote and Local File Maps
 	remoteFileMap, err := a.GetRemoteFileMap(exclMap, remotePath)
 	if err != nil {
-		return lFdiff, errors.Wrap(err, "error getting list dir from remote.")
+		return lFdiff, errors.Wrap(err, "Error retrieving remote directory list.")
 	}
 
-	// 4. Get flat file list on the local filesystem
 	localRootPath = strings.TrimRight(localRootPath, "/")
 	localFileList, err := getLocalFileMap(localRootPath, localFileFilters, exclMap)
 	if err != nil {
-		return lFdiff, errors.Wrap(err, "error getting list dir from local.")
+		return lFdiff, errors.Wrap(err, "Error retrieving local directory list.")
 	}
 
-	// 5. Get the file diff with operation
+	// 4. Compute File Differences
 	lFdiff = findDelta(remoteFileMap, localFileList, prevRemoteFileMap, localRootPath)
-	l.Logger.Debug("Diff: ", lFdiff)
 	return lFdiff, nil
 }
 
