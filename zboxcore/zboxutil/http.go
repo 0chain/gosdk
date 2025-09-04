e2872236b50fe9ba7bb6820e31ad51b0994d3383
naveen.p <mr.naveenseven@gmail.com>
2025-09-05 00:05:40 +0530

    fix: standardize public share HTTP request builders
    - Refactor NewCheckPublicShareExistsRequest to use joinUrl() and setClientInfoWithSign()
    - Refactor NewGetPublicShareRecipientsRequest to use joinUrl() and setClientInfoWithSign()
    - Refactor NewRemovePublicShareRecipientRequest to use joinUrl() and setClientInfoWithSign()

--- zboxcore/zboxutil/http.go
+++ zboxcore/zboxutil/http.go
@@ -85,6 +85,10 @@ const (
 	COLLABORATOR_ENDPOINT        = "/v1/file/collaborator/"
 	CALCULATE_HASH_ENDPOINT      = "/v1/file/calculatehash/"
 	SHARE_ENDPOINT               = "/v1/marketplace/shareinfo/"
+	PUBLIC_SHARE_ENDPOINT 			 = "/v1/marketplace/shareinfo/public/"
+	PUBLIC_SHARE_RECIPIENT_ENDPOINT  = "/v1/marketplace/shareinfo/public/recipient/"
+	PUBLIC_SHARE_CHECK_ENDPOINT 	 = "/v1/marketplace/shareinfo/public/check/"
+	PUBLIC_SHARE_RECIPIENTS_ENDPOINT = "/v1/marketplace/shareinfo/public/recipients/"
 	DIR_ENDPOINT                 = "/v1/dir/"
 	PLAYLIST_LATEST_ENDPOINT     = "/v1/playlist/latest/"
 	PLAYLIST_FILE_ENDPOINT       = "/v1/playlist/file/"
@@ -891,7 +895,7 @@ func NewRevokeShareRequest(baseUrl, allocationID, allocationTx, sig string, quer
 }
 
 func NewRevokePublicShareRequest(baseUrl, allocationID, allocationTx, sig string, query *url.Values, clients ...string) (*http.Request, error) {
-	u, err := joinUrl(baseUrl, SHARE_ENDPOINT, allocationTx)
+	u, err := joinUrl(baseUrl, PUBLIC_SHARE_ENDPOINT, allocationTx)
 	if err != nil {
 		return nil, err
 	}
@@ -913,15 +917,12 @@ func NewRevokePublicShareRequest(baseUrl, allocationID, allocationTx, sig string
 
 // NewCheckPublicShareExistsRequest creates a new HTTP request to check if public share exists
 func NewCheckPublicShareExistsRequest(baseUrl, allocationID, allocationTx, sig string, query *url.Values, clients ...string) (*http.Request, error) {
-	u, err := joinUrl(baseUrl, "/v1/marketplace/shareinfo/public/check/", allocationID)
+	u, err := joinUrl(baseUrl, PUBLIC_SHARE_CHECK_ENDPOINT, allocationTx)
 	if err != nil {
 		return nil, err
 	}
-	if query != nil {
-		u.RawQuery = query.Encode()
-	}
-
-	req, err := http.NewRequest("GET", u.String(), nil)
+	u.RawQuery = query.Encode()
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -936,15 +937,12 @@ func NewCheckPublicShareExistsRequest(baseUrl, allocationID, allocationTx, sig s
 
 // NewGetPublicShareRecipientsRequest creates a new HTTP request to get public share recipients
 func NewGetPublicShareRecipientsRequest(baseUrl, allocationID, allocationTx, sig string, query *url.Values, clients ...string) (*http.Request, error) {
-	u, err := joinUrl(baseUrl, "/v1/marketplace/shareinfo/public/recipients/", allocationID)
+	u, err := joinUrl(baseUrl, PUBLIC_SHARE_RECIPIENTS_ENDPOINT, allocationTx)
 	if err != nil {
 		return nil, err
 	}
-	if query != nil {
-		u.RawQuery = query.Encode()
-	}
-
-	req, err := http.NewRequest("GET", u.String(), nil)
+	u.RawQuery = query.Encode()
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -959,15 +957,12 @@ func NewGetPublicShareRecipientsRequest(baseUrl, allocationID, allocationTx, sig
 
 // NewRemovePublicShareRecipientRequest creates a new HTTP request to remove a public share recipient
 func NewRemovePublicShareRecipientRequest(baseUrl, allocationID, allocationTx, sig string, query *url.Values, clients ...string) (*http.Request, error) {
-	u, err := joinUrl(baseUrl, "/v1/marketplace/shareinfo/public/recipient/", allocationID)
+	u, err := joinUrl(baseUrl, PUBLIC_SHARE_RECIPIENT_ENDPOINT, allocationTx)
 	if err != nil {
 		return nil, err
 	}
-	if query != nil {
-		u.RawQuery = query.Encode()
-	}
-
-	req, err := http.NewRequest("DELETE", u.String(), nil)
+	u.RawQuery = query.Encode()
+	req, err := http.NewRequest(http.MethodDelete, u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
