0chain/gosdk

commit df4dbdf40bf88835b8010ea8d021b9c7c147ac15

client id support in zauth sign

core/client/zauth.go
@@ -535,7 +535,7 @@ func CallZvaultRetrieveSharedWallets(serverAddr, token string) (string, error) {
 
 // ZauthSignTxn returns a function that sends a txn signing request to the zauth server
 func ZauthSignTxn(serverAddr string) sys.AuthorizeFunc {
-	return func(msg string) (string, error) {
+	return func(msg string, clientIds ...string) (string, error) {
 		req, err := http.NewRequest("POST", serverAddr+"/sign/txn", bytes.NewBuffer([]byte(msg)))
 		if err != nil {
 			return "", errors.Wrap(err, "failed to create HTTP request")
@@ -543,6 +543,14 @@ func ZauthSignTxn(serverAddr string) sys.AuthorizeFunc {
 		req.Header.Set("Content-Type", "application/json")
 		c := GetClient()
 		pubkey := c.Keys[0].PublicKey
+		if len(clientIds) > 0 {
+			c = GetWalletByClientID(clientIds[0])
+			if c == nil {
+				return "", errors.Errorf("wallet not found for client ID: %s", clientIds[0])
+			}
+			pubkey = c.Keys[0].PublicKey
+		}
+
 		req.Header.Set("X-Peer-Public-Key", pubkey)
 
 		client := &http.Client{}
@@ -572,7 +580,7 @@ func ZauthSignTxn(serverAddr string) sys.AuthorizeFunc {
 }
 
 func ZauthAuthCommon(serverAddr string) sys.AuthorizeFunc {
-	return func(msg string) (string, error) {
+	return func(msg string, clientIds ...string) (string, error) {
 		req, err := http.NewRequest("POST", serverAddr+"/sign/msg", bytes.NewBuffer([]byte(msg)))
 		if err != nil {
 			return "", errors.Wrap(err, "failed to create HTTP request")
@@ -580,6 +588,14 @@ func ZauthAuthCommon(serverAddr string) sys.AuthorizeFunc {
 
 		c := GetClient()
 		pubkey := c.Keys[0].PublicKey
+		if len(clientIds) > 0 {
+			c = GetWalletByClientID(clientIds[0])
+			if c == nil {
+				return "", errors.Errorf("wallet not found for client ID: %s", clientIds[0])
+			}
+			pubkey = c.Keys[0].PublicKey
+		}
+
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("X-Peer-Public-Key", pubkey)
 

core/client/set.go
@@ -73,7 +73,7 @@ func init() {
 		if !wallet.IsSplit {
 			return sys.Sign(hash, client.signatureScheme, GetClientSysKeys(clients...))
 		}
-
+		fmt.Printf("Sign: wallet details: %+v\n", *wallet)
 		// get sign lock
 		<-sigC
 		fmt.Println("Sign: with sys.SignWithAuth:", sys.SignWithAuth, "sysKeys:", GetClientSysKeys(clients...))
@@ -128,7 +128,7 @@ func signHashWithAuth(hash, signatureScheme string, keys []sys.KeyPair, clientId
 		return "", errors.New("authCommon is not set")
 	}
 
-	rsp, err := sys.AuthCommon(string(data))
+	rsp, err := sys.AuthCommon(string(data), clientID)
 	if err != nil {
 		return "", err
 	}

core/sys/sign.go
@@ -16,4 +16,4 @@ type VerifyFunc func(signature string, msg string) (bool, error)
 
 type VerifyWithFunc func(pk, signature string, msg string) (bool, error)
 
-type AuthorizeFunc func(msg string) (string, error)
+type AuthorizeFunc func(msg string, clientIds... string) (string, error)

zboxcore/zboxutil/http.go
@@ -225,6 +225,7 @@ func setClientInfoWithSign(req *http.Request, sig, allocation, baseURL string, c
 	if wallet == nil {
 		return errors.New("wallet not found", clientID)
 	}
+	fmt.Printf("setClientInfoWithSign: wallet details: %+v\n", *wallet)
 	req.Header.Set("X-App-Client-ID", wallet.ClientID)
 	req.Header.Set("X-App-Client-Key", wallet.ClientKey)
 	req.Header.Set(CLIENT_SIGNATURE_HEADER, sig)

commit f79678dd111f478045233273046d94b41462ec3a

fix: use PeerPublicKey instead of Keys[0].PublicKey for zauth X-Peer-Public-Key header
Keys[0].PublicKey is the user's split key public key (c0), but zauth
stores and looks up by the peer's public key (c1). Using the wrong key
causes 'wallet is not connected' errors on every split-key sign request.

Bug introduced in df4dbdf4 (client id support in zauth sign).

core/client/zauth.go
@@ -542,13 +542,13 @@ func ZauthSignTxn(serverAddr string) sys.AuthorizeFunc {
 		}
 		req.Header.Set("Content-Type", "application/json")
 		c := GetClient()
-		pubkey := c.Keys[0].PublicKey
+		pubkey := c.PeerPublicKey
 		if len(keys) > 0 {
 			c = GetWalletByKey(keys[0])
 			if c == nil {
 				return "", errors.Errorf("wallet not found for pubkey: %s", keys[0])
 			}
-			pubkey = c.Keys[0].PublicKey
+			pubkey = c.PeerPublicKey
 		}
 
 		req.Header.Set("X-Peer-Public-Key", pubkey)
@@ -587,13 +587,13 @@ func ZauthAuthCommon(serverAddr string) sys.AuthorizeFunc {
 		}
 
 		c := GetClient()
-		pubkey := c.Keys[0].PublicKey
+		pubkey := c.PeerPublicKey
 		if len(keys) > 0 {
 			c = GetWalletByKey(keys[0])
 			if c == nil {
 				return "", errors.Errorf("multi-wallet-settings err: %v", keys[0])
 			}
-			pubkey = c.Keys[0].PublicKey
+			pubkey = c.PeerPublicKey
 		}
 
 		req.Header.Set("Content-Type", "application/json")